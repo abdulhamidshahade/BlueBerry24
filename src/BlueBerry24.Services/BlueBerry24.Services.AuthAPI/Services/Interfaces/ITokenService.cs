83e87dae4e3143a79af36615507bdfd277f3bc01
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:49:32 +0300

    Add ITokenService interface for token generation and managements

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using BlueBerry24.Services.AuthAPI.Models;
+
+namespace BlueBerry24.Services.AuthAPI.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(ApplicationUser user);
+    }
+}
