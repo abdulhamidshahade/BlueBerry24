642d511ad16c22afee49821538bf0329faf98db3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:03:30 +0300

    Add ApplicationDbContext for managing Identity-related database operations in AuthAPI - Define ApplicationDbContext inheriting from IdentityDbContext. - Configure ApplicationUser and ApplicationRole with integer-based keys. - Include a placeholder for custom model configurations in OnModelCreating.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlueBerry24.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueBerry24.Services.AuthAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
