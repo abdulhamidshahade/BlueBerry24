6c0178474ac705ee7c81156d518fa73c7b68efe8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:57:14 +0300

    Add AuthController to AuthAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueBerry24.Services.AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
