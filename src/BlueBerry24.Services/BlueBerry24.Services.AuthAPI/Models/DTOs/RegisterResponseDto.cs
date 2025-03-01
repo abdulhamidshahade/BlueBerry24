2bc24305e7d4050e25a3bfe838b62e0b22c1461a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:35:09 +0300

    Add RegisterResponseDto for registration response

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Models/DTOs/RegisterResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Services.AuthAPI.Models.DTOs
+{
+    public class RegisterResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+        public ApplicationUserDto User { get; set; }
+    }
+}
