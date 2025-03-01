1831d67b89a882d99edb87374647b232aca6ad9a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-01 23:50:57 +0300

    Add ApplicationRole custom role model to AuthAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Models/ApplicationRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueBerry24.Services.AuthAPI.Models
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public ApplicationRole()
+        {
+
+        }
+
+        public ApplicationRole(string role) : base(role)
+        {
+
+        }
+    }
+}
