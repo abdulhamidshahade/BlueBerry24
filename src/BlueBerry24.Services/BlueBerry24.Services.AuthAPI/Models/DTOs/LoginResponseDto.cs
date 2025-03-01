ddb7fd8bb3655b57a2afba25dd30034dff8ba806
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:34:16 +0300

    Add LoginResponseDto for user authentication response

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Models/DTOs/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.AuthAPI.Models.DTOs
+{
+    public class LoginResponseDto
+    {
+        public ApplicationUserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
