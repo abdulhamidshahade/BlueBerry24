581afa280ba4e5d58d187c30ad8355a0add9786e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:40:07 +0300

    Add IAuthService interface for authentication

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using BlueBerry24.Services.AuthAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.AuthAPI.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<RegisterResponseDto> Register(RegisterRequestDto requestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto requestDto);
+    }
+}
