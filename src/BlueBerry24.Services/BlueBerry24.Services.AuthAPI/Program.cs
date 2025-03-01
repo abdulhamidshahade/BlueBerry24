e141bf1595ac0b88b59acc167aa9cb32133a23c4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:08:35 +0300

    Configure ApplicationDbContext and Identity for SQL Server - Add DbContext configuration using SQL Server connection string. - Integrate Identity with custom ApplicationUser and ApplicationRole. - Enable default token providers for Identity.

--- src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Program.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Program.cs
@@ -1,3 +1,8 @@
+using BlueBerry24.Services.AuthAPI.Data;
+using BlueBerry24.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServerConnectionString"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
