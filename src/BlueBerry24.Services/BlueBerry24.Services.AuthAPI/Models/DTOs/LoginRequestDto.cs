a8d671affe839e77fed621322055e662bf9bf715
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-02 00:32:41 +0300

    Add LoginRequestDto for user login

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Models/DTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.AuthAPI.Models.DTOs
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
