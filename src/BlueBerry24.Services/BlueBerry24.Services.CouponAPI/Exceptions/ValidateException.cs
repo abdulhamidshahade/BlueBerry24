9e3b479bd169111f3878e17d59cfcaaf9286f439
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:39:23 +0300

    Add custom class to ValidateException for passing error messages - Add a constructor to the ValidateException class that accepts a string message parameter. - Invoke the base Exception class constructor to initialize the exception with the provided message.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Exceptions/ValidateException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.CouponAPI.Exceptions
+{
+    public class ValidateException : Exception
+    {
+        public ValidateException(string message) : base(message)
+        {
+
+        }
+    }
+}
