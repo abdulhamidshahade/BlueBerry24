f9b72fbe694ca8378238ca15aef2bebd16a5716a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:21:47 +0300

    Add ApplicationDbContext for managing database operations in CouponAPI - Define ApplicationDbContext inheriting from DbContext. - Include a placeholder for custom model configurations in OnModelCreating.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlueBerry24.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueBerry24.Services.CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
