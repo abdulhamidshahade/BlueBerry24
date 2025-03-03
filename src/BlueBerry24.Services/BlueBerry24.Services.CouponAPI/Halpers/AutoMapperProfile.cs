0ee47a6bcb9c0cfe801b927157bc7d0091324c1a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:09:46 +0300

    Add AutoMapper profile for Coupon to CouponDto mapping

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Halpers/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.CouponAPI.Models.DTOs;
+using BlueBerry24.Services.CouponAPI.Models;
+
+namespace BlueBerry24.Services.CouponAPI.Halpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Coupon, CouponDto>().ReverseMap();
+        }
+    }
+}
