4d4e6883a1d84f0e25c65a01bb06442ed7f129f4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:49:54 +0300

    Implement UnitOfWork for managing database transactions and cleanup in CouponAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Services/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using BlueBerry24.Services.CouponAPI.Data;
+using BlueBerry24.Services.CouponAPI.Services.Interfaces;
+
+namespace BlueBerry24.Services.CouponAPI.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
