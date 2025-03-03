b56aa15fb9e43e9cfb62f48e30f83579f41eb467
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:03:54 +0300

    Add Coupon domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal MinimumAmount { get; set; }
+    }
+}
