242b6cf5a2ffc2761e8f0acf8fa2d127c152d7c3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:38:22 +0300

    Add custom class for DuplicateEntityException to pass custom error messages - Implement a constructor in DuplicateEntityException class that accepts a string message parameter. - Call the base class constructor to initialize the exception with the provided message.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.CouponAPI.Exceptions
+{
+    public class DuplicateEntityException : Exception
+    {
+        public DuplicateEntityException(string message) : base(message)
+        {
+
+        }
+    }
+}
