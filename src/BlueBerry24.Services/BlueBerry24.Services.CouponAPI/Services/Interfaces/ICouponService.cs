6f64df344881e5d4c2df39c143f8d2b4ba264915
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-03 15:47:32 +0300

    Add ICouponService interface for managing coupon operations

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.CouponAPI/Services/Interfaces/ICouponService.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Services.CouponAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.CouponAPI.Services.Interfaces
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetByIdAsync(int id);
+        Task<CouponDto> GetByCodeAsync(string code);
+        Task<IEnumerable<CouponDto>> GetAllAsync();
+        Task<CouponDto> CreateAsync(CouponDto couponDto);
+        Task<CouponDto> UpdateAsync(int id, CouponDto couponDto);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsByCodeAsync(string code);
+    }
+}
