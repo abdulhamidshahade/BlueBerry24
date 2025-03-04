b702974550cf765303e9cafc02cc520768b83ea7
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:47:29 +0300

    Add category domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Models
+{
+    public class Category : CategoryBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
