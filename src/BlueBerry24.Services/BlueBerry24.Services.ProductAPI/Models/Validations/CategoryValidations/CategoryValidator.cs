110ad0dac4ea0e6b7ee08c9a484cdb79b42a205d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:53:55 +0300

    Add CategoryValidator for validate Category model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/CategoryValidations/CategoryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.CategoryValidations
+{
+    public class CategoryValidator : CategoryBaseValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
