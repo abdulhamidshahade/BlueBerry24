d73d237ae274708b2aa84e3020bd710f3a642592
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:53:46 +0300

    Add CategoryDtoValidator for validate CategoryDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/CategoryValidations/CategoryDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.CategoryValidations
+{
+    public class CategoryDtoValidator : CategoryBaseValidator<CategoryDto>
+    {
+        public CategoryDtoValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
