aee1697d8ce6ea318613c9ca1a00a80543f5352e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:54:13 +0300

    Add DeleteCategoryValidator for validate DeleteCategoryDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/CategoryValidations/DeleteCategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.CategoryValidations
+{
+    public class DeleteCategoryValidator : CategoryBaseValidator<DeleteCategoryDto>
+    {
+        public DeleteCategoryValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
