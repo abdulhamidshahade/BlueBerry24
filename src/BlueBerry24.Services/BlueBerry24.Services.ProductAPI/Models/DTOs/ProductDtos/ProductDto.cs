960b48595d15c34e7c9cc1c73f41cc21fd8a7bfc
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:53:04 +0300

    Add ProductDto to ProductAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/DTOs/ProductDtos/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos
+{
+    public class ProductDto : ProductBase
+    {
+        public int Id { get; set; }
+    }
+}
