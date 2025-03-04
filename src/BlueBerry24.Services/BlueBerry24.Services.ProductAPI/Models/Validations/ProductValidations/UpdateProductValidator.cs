3a7f67c5f5db512c4daed106da4fd588051469f3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:55:31 +0300

    Add UpdateProductValidator for validate UpdateProductDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/ProductValidations/UpdateProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.ProductValidations
+{
+    public class UpdateProductValidator : ProductBaseValidator<UpdateProductDto>
+    {
+        public UpdateProductValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
