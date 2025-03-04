498dd008adae74b8fa76cf25f93ff22bd4ddbe94
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:51:36 +0300

    Add CategoryDto to ProductAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/DTOs/CategoryDtos/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos
+{
+    public class CategoryDto : CategoryBase
+    {
+        public int Id { get; set; }
+    }
+}
