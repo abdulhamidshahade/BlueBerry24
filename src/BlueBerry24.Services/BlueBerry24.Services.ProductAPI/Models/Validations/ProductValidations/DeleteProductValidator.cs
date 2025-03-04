bc09b53aff5784dfa097f43951c5891df76c9f2f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:54:47 +0300

    Add DeleteProductValidator for validate DeleteProductDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/ProductValidations/DeleteProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.ProductValidations
+{
+    public class DeleteProductValidator : ProductBaseValidator<DeleteProductDto>
+    {
+        public DeleteProductValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
