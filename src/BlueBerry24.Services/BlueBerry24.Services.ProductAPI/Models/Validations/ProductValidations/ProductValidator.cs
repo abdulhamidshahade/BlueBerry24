8159f6cede69694f6694c464b8ac11c7a64e3afe
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:55:16 +0300

    Add ProductValidator for validate Product model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/ProductValidations/ProductValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.ProductValidations
+{
+    public class ProductValidator : ProductBaseValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("ID is required!");
+        }
+    }
+}
