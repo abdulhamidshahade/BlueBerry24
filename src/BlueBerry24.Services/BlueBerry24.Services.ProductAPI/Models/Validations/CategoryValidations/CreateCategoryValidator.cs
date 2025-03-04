7671dc57dfd0a2954c4186a93a1b568f88f14c58
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:54:04 +0300

    Add CreateCategoryValidator for validate CreateCategoryDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/CategoryValidations/CreateCategoryValidator.cs
@@ -0,0 +1,12 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.CategoryValidations
+{
+    public class CreateCategoryValidator : CategoryBaseValidator<CreateCategoryDto>
+    {
+        public CreateCategoryValidator()
+        {
+
+        }
+    }
+}
