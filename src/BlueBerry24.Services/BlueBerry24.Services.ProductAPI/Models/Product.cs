55f6c1bb8c4aedb0e1f8ebf35ebd05178c9680a4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:47:43 +0300

    Add product domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Models
+{
+    public class Product : ProductBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
