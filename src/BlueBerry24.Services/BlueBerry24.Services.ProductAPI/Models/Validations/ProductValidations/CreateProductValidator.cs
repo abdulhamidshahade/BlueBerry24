a18fd998856a98897402078356f5feb4ba029050
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:54:39 +0300

    Add CreateProductValidator for validate CreateProductDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Validations/ProductValidations/CreateProductValidator.cs
@@ -0,0 +1,12 @@
+﻿using BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos;
+
+namespace BlueBerry24.Services.ProductAPI.Models.Validations.ProductValidations
+{
+    public class CreateProductValidator : ProductBaseValidator<CreateProductDto>
+    {
+        public CreateProductValidator()
+        {
+
+        }
+    }
+}
