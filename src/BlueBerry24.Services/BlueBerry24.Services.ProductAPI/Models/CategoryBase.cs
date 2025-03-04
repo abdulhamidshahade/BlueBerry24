7f7729343d459986c19c1f293af6301ee103535d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-04 23:46:00 +0300

    Add abstract CategoryBase class with shared properties for category-related entities and dtos

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/CategoryBase.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Models
+{
+    public abstract class CategoryBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
