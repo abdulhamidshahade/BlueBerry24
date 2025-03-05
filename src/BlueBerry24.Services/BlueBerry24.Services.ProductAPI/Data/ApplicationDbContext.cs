e77487e7137af4537047ada53db03617cfe4313c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-05 19:56:52 +0300

    Add ApplicationDbContext for managing database operations in ProductAPI - Define ApplicationDbContext inheriting from DbContext. - Include a placeholder for custom model configurations in OnModelCreating.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlueBerry24.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueBerry24.Services.ProductAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
