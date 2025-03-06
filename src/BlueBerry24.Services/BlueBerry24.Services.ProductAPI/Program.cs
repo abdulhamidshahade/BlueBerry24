9207897c057d14de4761e71afed7dba715eb215b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 08:01:35 +0300

    Configure Swagger UI to load at root URL (http://localhost:xxxx) for ProductAPI - Set 'RoutePrefix' to 'string.empty' or  to make Swagger UI accessible at the root of the application. - Update Swagger endpoint to '/swagger/v1/swagger.json'. - Ensure -Swagger UI loads at 'http://localhost:xxxx/swagger'.

--- src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Program.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Program.cs
@@ -7,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -19,7 +21,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "BlueBerry24 CouponAPI v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
