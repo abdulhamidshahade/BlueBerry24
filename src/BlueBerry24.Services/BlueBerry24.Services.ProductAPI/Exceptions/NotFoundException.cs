cfdff0065de7e11b481419a2cc05a4618e4f56ec
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 07:57:33 +0300

    Add custom class to NotFoundException for passing error messages in ProductAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
