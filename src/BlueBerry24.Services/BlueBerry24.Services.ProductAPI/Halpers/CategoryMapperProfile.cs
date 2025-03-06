870c2cb0f43a4f61e2b045443d66a0618c516043
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 08:12:08 +0300

    Add CategoryMapperProfile profile for Category mappings

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Halpers/CategoryMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.ProductAPI.Models;
+using BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos;
+using BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos;
+
+namespace BlueBerry24.Services.ProductAPI.Halpers
+{
+    public class CategoryMapperProfile : Profile
+    {
+        public CategoryMapperProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, DeleteCategoryDto>().ReverseMap();
+        }
+    }
+}
