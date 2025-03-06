48912d2b2cdfa6fc03e164b1e7323c3ec4fb6b1d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 07:41:49 +0300

    Add ProductCategories navigation property to Category model

--- src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Category.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/Category.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public List<ProductCategory> ProductCategories { get; set; }
     }
 }
