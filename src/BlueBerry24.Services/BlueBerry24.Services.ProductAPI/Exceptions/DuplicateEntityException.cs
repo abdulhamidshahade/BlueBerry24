294f9fd9f100a65e4bb564da1f21e8e835dd2e2e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 07:57:09 +0300

    Add custom class for DuplicateEntityException to pass custom error messages in ProductAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Exceptions
+{
+    public class DuplicateEntityException : Exception
+    {
+        public DuplicateEntityException(string message) : base(message)
+        {
+
+        }
+    }
+}
