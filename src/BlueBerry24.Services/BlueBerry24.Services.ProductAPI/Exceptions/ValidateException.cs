a9a36d11b88e6d92c1d97bc85c03a0e7f776d830
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-06 07:57:48 +0300

    Add custom class to ValidateException for passing error messages in ProductAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Exceptions/ValidateException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.ProductAPI.Exceptions
+{
+    public class ValidateException : Exception
+    {
+        public ValidateException(string message) : base(message)
+        {
+
+        }
+    }
+}
