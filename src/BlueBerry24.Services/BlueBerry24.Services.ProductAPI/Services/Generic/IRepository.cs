e0b9ae1fb2a7569a21a8792b11be25affcdb5e32
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-07 19:43:48 +0300

    Add generic repository interface for CRUD operations in ProductAPI - Introduce IRepository<T> interface in the BlueBerry24.Services.ProductAPI.Services.Generic namespace. - Define methods for common CRUD operations:   - GetByIdAsync: Retrieve an entity by its ID.   - GetAsync: Retrieve a single entity based on a predicate.   - GetAllAsync: Retrieve all entities.   - FindAsync: Retrieve a collection of entities matching a predicate.   - AddAsync: Add a new entity.   - Update: Update an existing entity.   - Delete: Remove an entity.   - ExistsAsync: Check if an entity exists based on a predicate. - The interface is constrained to class types using 'where T : class'.

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Services/Generic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace BlueBerry24.Services.ProductAPI.Services.Generic
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    }
+}
