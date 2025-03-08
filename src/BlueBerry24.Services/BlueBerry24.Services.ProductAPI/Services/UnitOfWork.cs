6e25be6d63c8a1da2ddf1f948aa296f7c151376e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-08 04:42:19 +0300

    Implement UnitOfWork for managing database transactions

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Services/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using BlueBerry24.Services.ProductAPI.Data;
+using BlueBerry24.Services.ProductAPI.Services.Interfaces;
+
+namespace BlueBerry24.Services.ProductAPI.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
