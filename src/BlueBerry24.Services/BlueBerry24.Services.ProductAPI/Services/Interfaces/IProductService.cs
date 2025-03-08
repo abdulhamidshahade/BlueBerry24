b40cfe691b8bb6c55d4d4ad3a968dfcfcc3b24d0
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-08 04:40:58 +0300

    Add IProductService interface for managing product operations

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using BlueBerry24.Services.ProductAPI.Models;
+using BlueBerry24.Services.ProductAPI.Models.DTOs.ProductDtos;
+
+namespace BlueBerry24.Services.ProductAPI.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductDto> GetByIdAsync(int id);
+        Task<ProductDto> GetByNameAsync(string name);
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+        Task<ProductDto> CreateAsync(CreateProductDto productDto);
+        Task<ProductDto> UpdateAsync(int id, UpdateProductDto productDto);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
