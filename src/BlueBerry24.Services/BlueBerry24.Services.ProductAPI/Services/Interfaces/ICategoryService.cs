fbef7f171afa307e8e63cf98233eec023bf082b0
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-08 04:40:32 +0300

    Add ICategoryService interface for managing category operations

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using BlueBerry24.Services.ProductAPI.Models;
+using BlueBerry24.Services.ProductAPI.Models.DTOs.CategoryDtos;
+
+namespace BlueBerry24.Services.ProductAPI.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<CategoryDto> GetByIdAsync(int id);
+        Task<CategoryDto> GetByNameAsync(string name);
+        Task<IEnumerable<CategoryDto>> GetAllAsync();
+        Task<CategoryDto> CreateAsync(CreateCategoryDto categoryDto);
+        Task<CategoryDto> UpdateAsync(int id, UpdateCategoryDto categoryDto);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
