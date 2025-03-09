b1a05c2b37230bd70f15a5f6c25bded9fa39ac0a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-09 23:53:59 +0300

    Add ProductsController to manage product related api endpoints

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlueBerry24.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
