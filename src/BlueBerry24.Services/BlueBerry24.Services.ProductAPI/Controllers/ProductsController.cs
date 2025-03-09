2e5e6ad1d8893e5c78c2c2770e9c756da1f70d91
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-09 23:57:40 +0300

    Add dependency injection for AutoMapper in ProductsController

--- src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Controllers/ProductsController.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using BlueBerry24.Services.ProductAPI.Data;
+﻿using AutoMapper;
+using BlueBerry24.Services.ProductAPI.Data;
 using BlueBerry24.Services.ProductAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ namespace BlueBerry24.Services.ProductAPI.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, IMapper mapper)
         {
             _productService = productService;
+            _mapper = mapper;
         }
     }
 }
