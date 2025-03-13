d9c2fed009bfd196748cbddcce4806497b473563
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 10:08:04 +0300

    Add Shop domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Models/Shop.cs
@@ -0,0 +1,19 @@
+﻿using BlueBerry24.Services.ShopAPI.Halpers.Constants;
+
+namespace BlueBerry24.Services.ShopAPI.Models
+{
+    public class Shop : ShopBase
+    {
+        public string Id { get; set; }
+       
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Shop()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
