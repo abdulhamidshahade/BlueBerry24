978d77bda29b15c6b4d7fce69608dd41cb848465
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 10:14:57 +0300

    Add DeleteShopValidator for validator DeleteShopDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Models/Validations/ShopValidations/DeleteShopValidator.cs
@@ -0,0 +1,15 @@
+﻿using BlueBerry24.Services.ShopAPI.Models.DTOs.ShopDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ShopAPI.Models.Validations.ShopValidations
+{
+    public class DeleteShopValidator : ShopBaseValidator<DeleteShopDto>
+    {
+        public DeleteShopValidator()
+        {
+            RuleFor(i => i.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
