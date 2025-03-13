b7e74a3fb4158565a51020776de61dbdf5bcb589
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 10:15:45 +0300

    Add ShopValidator for validate Shop domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Models/Validations/ShopValidations/ShopValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BlueBerry24.Services.ShopAPI.Models.Validations.ShopValidations
+{
+    public class ShopValidator : ShopBaseValidator<Shop>
+    {
+        public ShopValidator()
+        {
+            RuleFor(i => i.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
