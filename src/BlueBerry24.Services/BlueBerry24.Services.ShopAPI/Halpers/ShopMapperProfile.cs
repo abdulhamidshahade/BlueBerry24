ce7c034281844fe979782afa4504dddf50fb0114
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 19:26:30 +0300

    Add ShopAutoMapper profile

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Halpers/ShopMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.ShopAPI.Models;
+using BlueBerry24.Services.ShopAPI.Models.DTOs.ShopDtos;
+
+namespace BlueBerry24.Services.ShopAPI.Halpers
+{
+    public class ShopMapperProfile : Profile
+    {
+        public ShopMapperProfile()
+        {
+            CreateMap<Shop, ShopDto>().ReverseMap();
+            CreateMap<CreateShopDto, Shop>().ReverseMap();
+            CreateMap<UpdateShopDto, Shop>().ReverseMap();
+            CreateMap<DeleteShopDto, Shop>().ReverseMap();
+        }
+    }
+}
