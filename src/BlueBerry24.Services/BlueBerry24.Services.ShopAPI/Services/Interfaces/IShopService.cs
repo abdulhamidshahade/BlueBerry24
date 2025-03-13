d29c4b3577554cb4729b098999e27119265344cc
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 18:31:22 +0300

    Add IShopService interface for managing shop operations

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Services/Interfaces/IShopService.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Services.ShopAPI.Models.DTOs.ShopDtos;
+
+namespace BlueBerry24.Services.ShopAPI.Services.Interfaces
+{
+    public interface IShopService
+    {
+        Task<ShopDto> GetByIdAsync(string id);
+        Task<ShopDto> GetByNameAsync(string name);
+        Task<IEnumerable<ShopDto>> GetAllAsync();
+        Task<ShopDto> CreateAsync(CreateShopDto shopDto);
+        Task<ShopDto> UpdateAsync(string id, UpdateShopDto shopDto);
+        Task<bool> DeleteAsync(string id);
+        Task<bool> ExistsAsync(string id);
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
