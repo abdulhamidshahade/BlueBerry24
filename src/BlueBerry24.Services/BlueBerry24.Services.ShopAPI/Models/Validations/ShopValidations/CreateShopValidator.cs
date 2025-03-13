e51c9a56505a0226bef2c7fd89c4ea9bb56a215e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 10:14:40 +0300

    Add CreateShopValidator for validate CreateShopDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Models/Validations/ShopValidations/CreateShopValidator.cs
@@ -0,0 +1,10 @@
+﻿using BlueBerry24.Services.ShopAPI.Models.DTOs.ShopDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ShopAPI.Models.Validations.ShopValidations
+{
+    public class CreateShopValidator : ShopBaseValidator<CreateShopDto>
+    {
+
+    }
+}
