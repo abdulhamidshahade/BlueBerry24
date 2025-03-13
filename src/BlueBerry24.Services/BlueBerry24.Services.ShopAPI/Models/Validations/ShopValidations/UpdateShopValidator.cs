8186652752933b26edc189756d0d1cf92f59789b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-13 10:16:05 +0300

    Add UpdateShopValidator for validate UpdateShopDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShopAPI/Models/Validations/ShopValidations/UpdateShopValidator.cs
@@ -0,0 +1,15 @@
+﻿using BlueBerry24.Services.ShopAPI.Models.DTOs.ShopDtos;
+using FluentValidation;
+
+namespace BlueBerry24.Services.ShopAPI.Models.Validations.ShopValidations
+{
+    public class UpdateShopValidator : ShopBaseValidator<UpdateShopDto>
+    {
+        public UpdateShopValidator()
+        {
+            RuleFor(i => i.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
