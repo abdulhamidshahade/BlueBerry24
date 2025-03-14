da5397fc677c091a43d49cb06f2f31baf2df565c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:01:59 +0300

    Add DuplicateEntityException class to manage custom errors

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Exceptions
+{
+    public class DuplicateEntityException : Exception
+    {
+        public DuplicateEntityException(string message) : base(message)
+        {
+
+        }
+    }
+}
