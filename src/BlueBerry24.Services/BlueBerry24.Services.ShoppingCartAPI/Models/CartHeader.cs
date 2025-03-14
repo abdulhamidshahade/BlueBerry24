b3d236d10d6e5e29fecd0c9b02c17bb0cd2a2794
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 18:37:02 +0300

    Add CartHeader domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,13 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public decimal Discount { get; set; }
+        public string CouponCode { get; set; }
+        public decimal CartTotal { get; set; }
+
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+    }
+}
