068a6291efd11ebd4956b91ac5b6cf0de3c04dfa
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:11:46 +0300

    Initialize ShoppingCartAPI with Authentication, Swagger, and Service Configuration

--- src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Program.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Program.cs
@@ -1,3 +1,14 @@
+using BlueBerry24.Services.ShoppingCartAPI.Data;
+using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs.DTOs;
+using BlueBerry24.Services.ShoppingCartAPI.Services;
+using BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +17,68 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
-builder.Services.AddSwaggerGen();
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["ApiSettings:SecretKey"]);
+var audience = builder.Configuration["ApiSettings:Audience"];
+var issuer = builder.Configuration["ApiSettings:Issuer"];
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the jwt",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id  = JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[]{}
+        }
+    });
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServerConnectionString"));
+});
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        ValidateAudience = true
+    };
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
+
+builder.Services.AddHttpClient("Product", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:ProductAPIUrl"]));
+
+builder.Services.AddHttpClient("Coupons", c => c.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:CouponAPIUrl"]));
 
 var app = builder.Build();
 
@@ -24,6 +96,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
