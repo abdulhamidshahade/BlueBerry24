30991191efc8185e0046e769304b456c6df2a24b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 18:40:27 +0300

    Add CartHeaderDto in ShoppingCartAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Models/DTOs/CartHeaderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CartHeaderDto
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public decimal Discount { get; set; }
+        public string CouponCode { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
