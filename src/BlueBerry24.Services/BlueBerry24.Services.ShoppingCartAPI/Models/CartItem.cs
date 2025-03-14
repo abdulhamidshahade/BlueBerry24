c00c7e5b327d4609ba233283ba715497eab8c1b3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 18:37:26 +0300

    Add CartItems domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs.DTOs;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Models
+{
+    public class CartItem
+    {
+        public string Id { get; set; }
+        public string CartHeaderId { get; set; }
+        public CartHeader CartHeader { get; set; }
+
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
