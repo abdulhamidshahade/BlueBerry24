3cd7d72530705e488e8e2f2b3a46364c58e92de7
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:06:15 +0300

    Add CouponService class for implement ICouponService inteface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/CouponService.cs
@@ -0,0 +1,26 @@
+﻿using BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Services
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+
+        public CouponService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<bool> IsAvailableAsync(string userId)
+        {
+            var client = _httpClientFactory.CreateClient("Coupons");
+
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUsedByUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
