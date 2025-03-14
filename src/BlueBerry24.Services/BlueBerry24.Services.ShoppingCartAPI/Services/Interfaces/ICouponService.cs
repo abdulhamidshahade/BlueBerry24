3d0f6f2b58084c36dc1a44750b673c193521f1b6
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:03:22 +0300

    Add ICouponService interface to manage coupon operations in ShoppingCartAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/Interfaces/ICouponService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces
+{
+    public interface ICouponService
+    {
+        Task<bool> IsUsedByUserAsync(string userId);
+        Task<bool> IsAvailableAsync(string userId);
+    }
+}
