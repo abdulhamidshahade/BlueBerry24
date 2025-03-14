f9b871a454b95b8a3d324bf6816515f8baaa3101
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 18:40:41 +0300

    Add CartItemDto in ShoppingCartAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Models/DTOs/CartItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CartItemDto
+    {
+        public string Id { get; set; }
+        public string CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
