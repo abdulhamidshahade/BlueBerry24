3d443bdc11241d37bcb7f7bb5fcb2d33b8d8e895
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:02:12 +0300

    Add NotFoundException class to manage custom errors

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
