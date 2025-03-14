5ffe535e77f13986e76f432088301b58230c2e0b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:03:44 +0300

    Add IProductService interface to manage product operations in ShoppingCartAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs;
+using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs.DTOs;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+
+    }
+}
