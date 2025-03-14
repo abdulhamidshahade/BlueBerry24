adf09a1293b9f1a17c0bc5f3d7e9ad328f36dbce
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 18:39:42 +0300

    Add CouponDto in ShoppingCartAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Models/DTOs/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CouponDto
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal MinimumAmount { get; set; }
+        public string ShopId { get; set; }
+    }
+}
