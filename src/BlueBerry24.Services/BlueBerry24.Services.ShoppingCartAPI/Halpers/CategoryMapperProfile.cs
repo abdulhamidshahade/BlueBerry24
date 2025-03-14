0dee568ca31b9c9b45842f33b76d5cdff093871e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-14 23:01:26 +0300

    Add CategoryAutoMapper profile

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Halpers/CategoryMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.ShoppingCartAPI.Models;
+using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Halpers
+{
+    public class CategoryMapperProfile : Profile
+    {
+        public CategoryMapperProfile()
+        {
+            CreateMap<CartHeader, CartHeader>().ReverseMap();
+            CreateMap<CartItem, CartItemDto>().ReverseMap();
+        }
+    }
+}
