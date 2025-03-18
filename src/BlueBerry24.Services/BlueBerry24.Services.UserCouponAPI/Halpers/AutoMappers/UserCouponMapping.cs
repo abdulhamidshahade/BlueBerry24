275eb0ad9fc6bb591ba9226e60e43db04e039d05
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-18 03:15:59 +0300

    Add UserCouponMapping profile

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.UserCouponAPI/Halpers/AutoMappers/UserCouponMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.UserCouponAPI.Models;
+using BlueBerry24.Services.UserCouponAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.UserCouponAPI.Halpers.AutoMappers
+{
+    public class UserCouponMapping : Profile
+    {
+        public UserCouponMapping()
+        {
+            CreateMap<UserCoupon, UserCouponDto>().ReverseMap();
+        }
+    }
+}
