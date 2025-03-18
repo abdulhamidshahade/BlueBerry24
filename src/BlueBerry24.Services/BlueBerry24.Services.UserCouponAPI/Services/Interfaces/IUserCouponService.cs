a3f13ff3f9c5fd9ab956abcd2792bdba5c24d723
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-18 03:37:47 +0300

    Add IUserCouponService interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.UserCouponAPI/Services/Interfaces/IUserCouponService.cs
@@ -0,0 +1,13 @@
+﻿using BlueBerry24.Services.UserCouponAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.UserCouponAPI.Services.Interfaces
+{
+    public interface IUserCouponService
+    {
+        Task<UserCouponDto> AddCouponToUserAsync(string userId, string couponId);
+        Task<bool> DisableUserCouponAsync(string usreId, string couponId);
+        Task<IReadOnlyCollection<string>> GetCouponsByUserIdAsync(string userId);
+        Task<IReadOnlyCollection<string>> GetUsersByCouponIdAsync(string couponId);
+        Task<bool> HasUserUsedCouponAsync(string userId, string couponId);
+    }
+}
