54e8469e810b91e559ae145de8357e90e1958856
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-19 23:44:02 +0300

    Add CouponDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.UserCouponAPI/Models/DTOs/CouponDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.UserCouponAPI.Models.DTOs
+{
+    public class CouponDto
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+    }
+}
