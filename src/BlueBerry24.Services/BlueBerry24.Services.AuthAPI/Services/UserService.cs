9e03889496ed06d0d6ea6a8d8466b28d98daf9af
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-19 23:39:30 +0300

    Add UserService implmenets IUserService inerface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.AuthAPI/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using BlueBerry24.Services.AuthAPI.Models;
+using BlueBerry24.Services.AuthAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlueBerry24.Services.AuthAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<bool> IsUserExistsByEmailAsync(string emailAddress)
+        {
+            var user = await _userManager.FindByEmailAsync(emailAddress);
+
+            return user != null;
+        }
+
+        public async Task<bool> IsUserExistsByIdAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return user != null;
+        }
+    }
+}
