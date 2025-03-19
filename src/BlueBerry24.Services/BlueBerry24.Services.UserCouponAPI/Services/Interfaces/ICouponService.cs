665e730c314249c78d7f38c17fd7e65005917b0c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-19 23:07:44 +0300

    Add ICouponService interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.UserCouponAPI/Services/Interfaces/ICouponService.cs
@@ -0,0 +1,7 @@
+﻿namespace BlueBerry24.Services.UserCouponAPI.Services.Interfaces
+{
+    public interface ICouponService
+    {
+        Task<bool> IsCouponExistsByIdAsync(string couponId);
+    }
+}
