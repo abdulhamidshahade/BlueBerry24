74450daf48e926669a26145b8d98e7af538df816
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-20 00:21:01 +0300

    Add ShopId property to Product model model

--- src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/ProductBase.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.ProductAPI/Models/ProductBase.cs
@@ -7,5 +7,7 @@
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public string ImageUrl { get; set; }
+
+        public string ShopId { get; set; }
     }
 }
