a40b938334aecabafcb02350bc81c9b974e6bb77
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-20 00:29:03 +0300

    Add Stock domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Models/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueBerry24.Services.StockAPI.Models
+{
+    public class Stock
+    {
+        public string Id { get; set; }
+
+        public string ShopId { get; set; }
+        public string ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
