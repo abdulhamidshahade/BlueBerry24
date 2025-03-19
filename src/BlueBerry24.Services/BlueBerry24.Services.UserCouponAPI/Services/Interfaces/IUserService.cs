ea3f65acebbd4d607a0ead10ff34160607746c87
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-19 23:07:57 +0300

    Add IUserService interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.UserCouponAPI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.UserCouponAPI.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> IsUserExistsByIdAsync(string userId);
+        Task<bool> IsUserExistsByEmailAsync(string userId);
+    }
+}
