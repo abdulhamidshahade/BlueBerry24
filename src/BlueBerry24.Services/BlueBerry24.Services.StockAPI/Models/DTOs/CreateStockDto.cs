1179d1ef5e174db586a02026064783b9452995c0
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-20 00:33:42 +0300

    Add CreateStockDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Models/DTOs/CreateStockDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Services.StockAPI.Models.DTOs
+{
+    public class CreateStockDto
+    {
+        public string ShopId { get; set; }
+        public string ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
