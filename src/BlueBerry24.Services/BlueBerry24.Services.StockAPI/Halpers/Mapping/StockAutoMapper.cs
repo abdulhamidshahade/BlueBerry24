37741ceb0422e29510c0684122dde4904bd7f3c5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-20 00:33:55 +0300

    Add StockAutoMapper

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Halpers/Mapping/StockAutoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.StockAPI.Models;
+using BlueBerry24.Services.StockAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.StockAPI.Halpers.Mapping
+{
+    public class StockAutoMapper : Profile
+    {
+        public StockAutoMapper()
+        {
+            CreateMap<Stock, StockDto>().ReverseMap();
+            CreateMap<Stock, CreateStockDto>().ReverseMap();
+        }
+    }
+}
