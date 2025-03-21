9922b4b36780409be404aeb533d6d2d03d12fec2
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-21 23:45:49 +0300

    Add IStockService interface in StockAPI

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Services/Interfaces/IStockService.cs
@@ -0,0 +1,20 @@
+﻿using BlueBerry24.Services.StockAPI.Models;
+using BlueBerry24.Services.StockAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.StockAPI.Services.Interfaces
+{
+    public interface IStockService
+    {
+        Task<Stock> CreateStockAsync(CreateStockDto stockDto);
+        Task<bool> UpdateStockByIdAsync(string id, StockDto stockDto);
+        Task<bool> DeleteStockByIdAsync(string id);
+        Task<Stock> GetStockByIdAsync(string id);
+        Task<List<Stock>> GetStocksByShopIdAsync(string shopId);
+
+        Task CheckStock(string productId, string shopId, int quantity);
+
+        Task IncreaseByItemAsync(string productId, string shopId);
+        Task DecreaseByItemAsync(string productId, string shopId);
+
+    }
+}
