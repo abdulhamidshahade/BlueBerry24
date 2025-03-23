98436f103a8c383c39fe61b0c6a089528ac05c84
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-23 23:47:30 +0300

    Inject StockAPI services in DI container

--- src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Program.cs
+++ src/BlueBerry24.Services/BlueBerry24.Services.StockAPI/Program.cs
@@ -1,4 +1,6 @@
 using BlueBerry24.Services.StockAPI.Data;
+using BlueBerry24.Services.StockAPI.Services;
+using BlueBerry24.Services.StockAPI.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
 });
+
+builder.Services.AddHttpClient("Product", options => options.BaseAddress =
+new Uri(builder.Configuration["ApiUrls:ProductAPI"]));
+
+builder.Services.AddHttpClient("Shop", options => options.BaseAddress =
+new Uri(builder.Configuration["ApiUrls:ShopAPI"]));
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IShopService, ShopService>();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
