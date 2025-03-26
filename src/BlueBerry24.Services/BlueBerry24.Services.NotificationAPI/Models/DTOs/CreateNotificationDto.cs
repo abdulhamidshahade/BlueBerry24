d09ccd93524ceeabf8d0eacc550fabe3c489169b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-26 23:29:52 +0300

    Add CreateNotificationDto

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.NotificationAPI/Models/DTOs/CreateNotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Services.NotificationAPI.Data.Enums;
+
+namespace BlueBerry24.Services.NotificationAPI.Models.DTOs
+{
+    public class CreateNotificationDto
+    {
+        public string RecipientId { get; set; }
+        public Recipient RecipientType { get; set; } = Recipient.Shop;
+        public NotificationType NotificationType { get; set; } = NotificationType.LowStockAlert;
+        public string Message { get; set; }
+
+        public Dictionary<string, object> Metadata { get; set; }
+
+        public NotificationStatus NotificationStatus { get; set; } = NotificationStatus.Unread;
+    }
+}
