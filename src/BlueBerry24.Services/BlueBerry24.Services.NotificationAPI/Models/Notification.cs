151ca03954e818c4acf6d617ea92ac59e2243416
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-26 23:29:22 +0300

    Add Notification domain model

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.NotificationAPI/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿using BlueBerry24.Services.NotificationAPI.Data.Enums;
+
+namespace BlueBerry24.Services.NotificationAPI.Models
+{
+    public class Notification
+    {
+        public string Id { get; set; }
+
+        public string RecipientId { get; set; }
+        public Recipient RecipientType { get; set; } = Recipient.Shop;
+        public NotificationType NotificationType { get; set; } = NotificationType.LowStockAlert;
+        public string Message { get; set; }
+
+        public Dictionary<string, object> Metadata { get; set; } = new Dictionary<string, object>();
+
+        public NotificationStatus NotificationStatus { get; set; } = NotificationStatus.Unread;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime ExpirationDate { get; set; }
+
+
+    }
+}
