4ec0e1be5cfeae419baaf2dedfde4075ffd442c6
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-26 23:30:17 +0300

    Add NotificationAutoMapping

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.NotificationAPI/Halpers/AutoMapper/NotificationAutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlueBerry24.Services.NotificationAPI.Models;
+using BlueBerry24.Services.NotificationAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.NotificationAPI.Halpers.AutoMapper
+{
+    public class NotificationAutoMapping : Profile
+    {
+        public NotificationAutoMapping()
+        {
+            CreateMap<Notification, NotificationDto>().ReverseMap();
+        }
+    }
+}
