2d4b3ed5b468c1066e1e6805d3156b50f0d8f80b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-18 02:57:34 +0300

    Add RedisDistributedLockService

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/RedisDistributedLockService.cs
@@ -0,0 +1,25 @@
+﻿using BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces;
+
+using StackExchange.Redis;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Services
+{
+    public class RedisDistributedLockService : IDistributedLockService
+    {
+        private readonly IDatabase _db;
+
+        public RedisDistributedLockService(IConnectionMultiplexer redis)
+        {
+            _db = redis.GetDatabase();
+        }
+        public async Task<bool> AcquireLockAsync(string key, string value, TimeSpan expiry)
+        {
+            return await _db.LockTakeAsync(key, value, expiry);
+        }
+
+        public async Task<bool> ReleaseLockAsync(string key, string value)
+        {
+            return await _db.LockReleaseAsync(key, value);
+        }
+    }
+}
