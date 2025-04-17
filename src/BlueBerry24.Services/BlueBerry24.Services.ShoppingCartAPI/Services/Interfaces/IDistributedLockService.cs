6c134c775c9e1564401699479d930bb022589886
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-18 02:00:18 +0300

    Add IDistributedLockService interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/Interfaces/IDistributedLockService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces
+{
+    public interface IDistributedLockService
+    {
+        Task<bool> AcquireLockAsync(string key, string value, TimeSpan expiry);
+        Task<bool> ReleaseLockAsync(string key, string value);
+    }
+}
