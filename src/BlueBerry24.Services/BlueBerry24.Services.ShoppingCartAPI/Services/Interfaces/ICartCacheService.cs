da751746b70f7d80d7a19f9f6db6b3bf4c44f3a5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-18 02:09:10 +0300

    Add ICartCacheService interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Services/Interfaces/ICartCacheService.cs
@@ -0,0 +1,10 @@
+﻿using BlueBerry24.Services.ShoppingCartAPI.Models.DTOs;
+
+namespace BlueBerry24.Services.ShoppingCartAPI.Services.Interfaces
+{
+    public interface ICartCacheService
+    {
+        Task<CartDto> GetCartAsync(string userId);
+        Task SetCartAsync(string userId, CartDto cart);
+    }
+}
