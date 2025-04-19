4afd4997bddd60d92781a263725ea26c9a798c4f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-19 04:43:54 +0300

    Add IStockRpcClient interface

---
+++ src/BlueBerry24.Services/BlueBerry24.Services.ShoppingCartAPI/Messaging/Client/Interfaces/IStockRpcClient.cs
@@ -0,0 +1,7 @@
+﻿namespace BlueBerry24.Services.ShoppingCartAPI.Messaging.Client.Interfaces
+{
+    public interface IStockRpcClient : IDisposable
+    {
+        Task<bool> IsProductAvailableInStockAsync(string productId, string shopId);
+    }
+}
