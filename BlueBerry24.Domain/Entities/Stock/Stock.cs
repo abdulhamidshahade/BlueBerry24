5907f2ea16178cef0d8650027c2d618f4ba072ce
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 17:15:53 +0300

    Add Stock entities

---
+++ BlueBerry24.Domain/Entities/Stock/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace BlueBerry24.Domain.Entities.Stock
+{
+    public class Stock
+    {
+        public string Id { get; set; }
+
+        public string ShopId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
