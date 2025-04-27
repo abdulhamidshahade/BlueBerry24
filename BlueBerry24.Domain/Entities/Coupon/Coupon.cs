036e1e9d45aff05e67ae0c951070acd7da0a5ace
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 16:36:41 +0300

    Add Coupon entities

---
+++ BlueBerry24.Domain/Entities/Coupon/Coupon.cs
@@ -0,0 +1,16 @@
+﻿namespace BlueBerry24.Domain.Entities.Coupon
+{
+    public class Coupon
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal MinimumAmount { get; set; }
+        public bool IsActive { get; set; }
+
+        public Coupon()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
