cbb22a3be0e580535e0aa343a7088be8cbd1b689
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 19:32:38 +0300

    Add Auth repository interfaces

---
+++ BlueBerry24.Domain/Repositories/Interfaces/Auth/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlueBerry24.Domain.Entities.Auth;
+
+namespace BlueBerry24.Domain.Repositories.Interfaces.Auth
+{
+    public interface IAuthRepository
+    {
+        Task<ApplicationUser> Register(ApplicationUser requestModel);
+    }
+}
---
+++ BlueBerry24.Domain/Repositories/Interfaces/Auth/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Domain.Repositories.Interfaces.Auth
+{
+    public interface IUserRepository
+    {
+        Task<bool> IsUserExistsByIdAsync(string userId);
+        Task<bool> IsUserExistsByEmailAsync(string emailAddress);
+    }
+}
