937348cb69fe460a3d74c7cc09d34b6c95c62bea
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 17:13:11 +0300

    Add Shop entities

---
+++ BlueBerry24.Domain/Entities/Shop/ShopBase.cs
@@ -0,0 +1,18 @@
+﻿using BlueBerry24.Services.ShopAPI.Halpers.Constants;
+
+namespace BlueBerry24.Domain.Entities.Shop
+{
+    public abstract class ShopBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OwnerId { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string LogoUrl { get; set; }
+        public ShopStatus Status { get; set; } = ShopStatus.Active;
+    }
+}
