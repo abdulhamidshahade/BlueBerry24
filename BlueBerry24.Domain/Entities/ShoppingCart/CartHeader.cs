3bcaf6e2df43528c480f4dbe89476ce6429799f4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 17:15:10 +0300

    Add ShoppingCart entities

---
+++ BlueBerry24.Domain/Entities/ShoppingCart/CartHeader.cs
@@ -0,0 +1,15 @@
+﻿namespace BlueBerry24.Domain.Entities.ShoppingCart
+{
+    public class CartHeader
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public decimal Discount { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+        public decimal CartTotal { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+    }
+}
---
+++ BlueBerry24.Domain/Entities/ShoppingCart/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BlueBerry24.Domain.Entities.ShoppingCart
+{
+    public class CartItem
+    {
+        public string Id { get; set; }
+        public string CartHeaderId { get; set; }
+        public CartHeader CartHeader { get; set; }
+
+        public string ProductId { get; set; }
+        public string ShopId { get; set; }
+
+        //[NotMapped]
+        //public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+    }
+}
