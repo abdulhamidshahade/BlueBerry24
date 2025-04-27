a69bfa595fd21a9cba5e14866e5cec4a21e3982f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 19:59:22 +0300

    Add Shop repository interfaces

---
+++ BlueBerry24.Domain/Repositories/ShopInterfaces/IShopRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Domain.Entities.Shop;
+
+namespace BlueBerry24.Domain.Repositories.ShopInterfaces
+{
+    public interface IShopRepository
+    {
+        Task<Shop> GetByIdAsync(string id);
+        Task<Shop> GetByNameAsync(string name);
+        Task<IEnumerable<Shop>> GetAllAsync();
+        Task<Shop> CreateAsync(Shop shop);
+        Task<Shop> UpdateAsync(string id, Shop shopDto);
+        Task<bool> DeleteAsync(string id);
+        Task<bool> ExistsAsync(string id);
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
