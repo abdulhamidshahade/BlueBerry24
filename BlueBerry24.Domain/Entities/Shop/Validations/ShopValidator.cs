66842040bd9d716118251bf0f73a6d2485d00fad
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 17:13:38 +0300

    Add Shop entities validation models

---
+++ BlueBerry24.Domain/Entities/Shop/Validations/ShopValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BlueBerry24.Domain.Entities.Shop.Validations
+{
+    public class ShopValidator : ShopBaseValidator<Shop>
+    {
+        public ShopValidator()
+        {
+            RuleFor(i => i.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
