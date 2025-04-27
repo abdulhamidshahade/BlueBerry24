c542dd37b3b995905c2a6434739e4cd0f108ed7f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 19:46:58 +0300

    Add Coupon repository interfaces

---
+++ BlueBerry24.Domain/Repositories/CouponInterfaces/ICouponRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Domain.Entities.Coupon;
+
+namespace BlueBerry24.Domain.Repositories.CouponInterfaces
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetByIdAsync(string id);
+        Task<Coupon> GetByCodeAsync(string code);
+        Task<IEnumerable<Coupon>> GetAllAsync();
+        Task<Coupon> CreateAsync(Coupon coupon);
+        Task<Coupon> UpdateAsync(string id, Coupon coupon);
+        Task<bool> DeleteAsync(string id);
+        Task<bool> ExistsByIdAsync(string id);
+        Task<bool> ExistsByCodeAsync(string code);
+    }
+}
