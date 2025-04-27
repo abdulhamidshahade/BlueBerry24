feb9e0195570a42f5e0b8daf8450dcd74fc64223
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-27 16:32:53 +0300

    Add Auth entities

---
+++ BlueBerry24.Domain/Entities/Auth/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueBerry24.Domain.Entities.Auth
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole()
+        {
+
+        }
+
+        public ApplicationRole(string role) : base(role)
+        {
+
+        }
+    }
+}
---
+++ BlueBerry24.Domain/Entities/Auth/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueBerry24.Domain.Entities.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
