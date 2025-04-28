1df69cb5774d77a3197aa699669885a96a8a716b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-28 16:32:31 +0300

    Update domain's shopping cart repository to provide ISP and SRP

---
+++ BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartHeaderRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories.ShoppingCartInterfaces
+{
+    public interface ICartHeaderRepository
+    {
+        Task<bool> ExistsByIdAsync(int id);
+        Task<CartHeader> CreateCartAsync(CartHeader header);
+        Task<bool> DeleteCartHeaderAsync(int id);
+        Task<CartHeader> UpdateCartHeaderAsync(int id, CartHeader header);
+    }
+}
---
+++ BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories.ShoppingCartInterfaces
+{
+    public interface ICartItemRepository
+    {
+        Task<CartItem> AddItemAsync(CartItem item);
+        Task<bool> RemoveItemAsync(CartItem item);
+        Task<CartItem> UpdateItemCountAsync(CartItem item, int newCount);
+
+        Task<bool> ExistsByUserIdAsync(int userId, int headerId);
+        Task<bool> IncreaseItemAsync(CartItem item);
+        Task<bool> DecreaseItemAsync(CartItem item);
+    }
+}
