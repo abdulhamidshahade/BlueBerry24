51e05b0d95b854153156210f21de636d7ee83462
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-30 22:32:19 +0300

    Add UserCouponDto

---
+++ BlueBerry24.Application/Dtos/CouponDtos/UserCouponDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.CouponDtos
+{
+    class UserCouponDto
+    {
+        public string UserId { get; set; }
+        public string CouponId { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
