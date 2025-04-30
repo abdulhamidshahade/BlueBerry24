fd8403c3a963c65e11dcd9e82747810bccff0367
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-30 22:58:04 +0300

    Add IRoleService interface

---
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿
+
+using BlueBerry24.Domain.Entities.Auth;
+
+namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
+{
+    public interface IRoleService
+    {
+        Task EnsureRoleExistsAsync(string roleName);
+        Task<bool> AssignRoleToUserAsync(ApplicationUser user, string roleName);
+        bool IsValidRole(string roleName);
+    }
+}
