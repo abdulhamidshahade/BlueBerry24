f8084bcb12db612aead4a765fd11a73f6b041fad
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-04-30 22:32:35 +0300

    Add ResponseDto for api response

---
+++ BlueBerry24.Application/Dtos/ResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos
+{
+    class ResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public object Data { get; set; }
+        public List<string> Errors { get; set; } = new();
+    }
+}
