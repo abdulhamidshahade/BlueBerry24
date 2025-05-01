443e869e39d132b9cf582860fca601b03b3ddcb6
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:59:08 +0300

    Add IProductService inerface

---
+++ BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using BlueBerry24.Application.Dtos.ProductDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces
+{
+    public interface IProductService
+    {
+        Task<ProductDto> GetByIdAsync(int id);
+        Task<ProductDto> GetByNameAsync(string name);
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+        Task<ProductDto> CreateAsync(CreateProductDto productDto, List<int> categories);
+        Task<ProductDto> UpdateAsync(int id, UpdateProductDto productDto, List<int> categories);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsByIdAsync(int id);
+        Task<bool> ExistsByNameAsync(string name);
+        //Task<bool> ExistsByShopIdAsync(string productId, string shopId);
+    }
+}
