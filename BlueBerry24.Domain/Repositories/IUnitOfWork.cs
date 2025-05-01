05eb626b5fe5db6aaa49533deaa9e2834c8533a6
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:46:52 +0300

    Add IUnitOfWork interface

---
+++ BlueBerry24.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+        Task<bool> SaveDbChangesAsync();
+    }
+}
