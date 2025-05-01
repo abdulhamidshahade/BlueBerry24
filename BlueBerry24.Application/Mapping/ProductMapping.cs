12b804aeb721924bcd25c238d8b56b89cb1808ad
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:53:51 +0300

    Add ProductMapping

---
+++ BlueBerry24.Application/Mapping/ProductMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.ProductDtos;
+using BlueBerry24.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductDto, CreateProductDto>().ReverseMap();
+            CreateMap<ProductDto, UpdateProductDto>().ReverseMap();
+            CreateMap<ProductDto, DeleteProductDto>().ReverseMap();
+            CreateMap<ProductDto, Product>().ReverseMap();
+        }
+    }
+}
