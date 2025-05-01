118325a53fd4e43a2e20e88fab56b753e6583aae
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:46:09 +0300

    Add IUserCouponRepository interface

---
+++ BlueBerry24.Domain/Repositories/CouponInterfaces/IUserCouponRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlueBerry24.Domain.Entities.Coupon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories.CouponInterfaces
+{
+    public interface IUserCouponRepository
+    {
+        Task<UserCoupon> AddCouponToUserAsync(int userId, int couponId);
+        Task<bool> DisableCouponToUserAsync(int usreId, int couponId);
+        Task<List<string>> GetCouponsByUserIdAsync(int userId);
+        Task<List<string>> GetUsersByCouponIdAsync(int couponId);
+        Task<bool> IsCouponUsedByUser(int userId, int couponId);
+    }
+}
