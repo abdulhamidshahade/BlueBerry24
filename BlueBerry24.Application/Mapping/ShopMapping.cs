93f2fdf30394e678168c1ce95b5c38d0181b5480
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:53:59 +0300

    Add ShopMapping

---
+++ BlueBerry24.Application/Mapping/ShopMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.ShopDtos;
+using BlueBerry24.Domain.Entities.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    class ShopMapping : Profile
+    {
+        public ShopMapping()
+        {
+            CreateMap<ShopDto, CreateShopDto>().ReverseMap();
+            CreateMap<ShopDto, UpdateShopDto>().ReverseMap();
+            CreateMap<ShopDto, DeleteShopDto>().ReverseMap();
+            CreateMap<ShopDto, Shop>().ReverseMap();
+        }
+    }
+}
