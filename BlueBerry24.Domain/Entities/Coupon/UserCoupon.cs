2e823c8b0e70144d8a86a15bd468aa873d534166
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:52:02 +0300

    Add UserCoupon join entity

---
+++ BlueBerry24.Domain/Entities/Coupon/UserCoupon.cs
@@ -0,0 +1,24 @@
+﻿using BlueBerry24.Domain.Entities.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.Coupon
+{
+    public class UserCoupon
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public int CouponId { get; set; }
+        public Coupon Coupon { get; set; }
+
+        public bool IsUsed { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
