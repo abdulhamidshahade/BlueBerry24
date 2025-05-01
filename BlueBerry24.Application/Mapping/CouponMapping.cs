720319d0358a4995a3175e39119439a15d4f0413
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:53:42 +0300

    Add CouponMapping

---
+++ BlueBerry24.Application/Mapping/CouponMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.CouponDtos;
+using BlueBerry24.Domain.Entities.Coupon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    class CouponMapping : Profile
+    {
+        public CouponMapping()
+        {
+            CreateMap<CouponDto, CreateCouponDto>().ReverseMap();
+            CreateMap<CouponDto, UpdateCouponDto>().ReverseMap();
+            CreateMap<CouponDto, DeleteCouponDto>().ReverseMap();
+            CreateMap<CouponDto, Coupon>().ReverseMap();
+        }
+    }
+}
