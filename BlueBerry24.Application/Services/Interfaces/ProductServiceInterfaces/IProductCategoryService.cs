d4be2910d86e42b2eb7902452ac0878cf439408d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:58:58 +0300

    Add ProductCategoryService interface

---
+++ BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Application.Dtos.ProductDtos;
+using BlueBerry24.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces
+{
+    public interface IProductCategoryService
+    {
+        Task<bool> AddProductCategoryAsync(ProductDto Product, List<int> categories);
+        Task<bool> UpdateProductCategoryAsync(ProductDto product, List<int> categories);
+    }
+}
