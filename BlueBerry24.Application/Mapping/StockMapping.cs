ee263a5d7024c42b393d72cdc54cb0bfdc203189
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:54:19 +0300

    Add StockMapping

---
+++ BlueBerry24.Application/Mapping/StockMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.StockDtos;
+using BlueBerry24.Domain.Entities.Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    class StockMapping : Profile
+    {
+        public StockMapping()
+        {
+            CreateMap<StockDto, CreateStockDto>().ReverseMap();
+            CreateMap<StockDto, UpdateStockDto>().ReverseMap();
+            CreateMap<StockDto, Stock>().ReverseMap();
+        }
+    }
+}
