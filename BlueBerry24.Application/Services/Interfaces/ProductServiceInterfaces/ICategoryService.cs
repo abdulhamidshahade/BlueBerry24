1266facc8ce7c3517ea443af62c792f353c873c3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:58:48 +0300

    Add ICategoryService interface

---
+++ BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Application.Dtos.CategoryDtos;
+
+namespace BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces
+{
+    public interface ICategoryService
+    {
+        Task<CategoryDto> GetByIdAsync(int id);
+        Task<CategoryDto> GetByNameAsync(string name);
+        Task<IEnumerable<CategoryDto>> GetAllAsync();
+        Task<CategoryDto> CreateAsync(CreateCategoryDto categoryDto);
+        Task<CategoryDto> UpdateAsync(int id, UpdateCategoryDto categoryDto);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
