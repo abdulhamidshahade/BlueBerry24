96485ffe80c3cdaa25bd35550e440ea8bf018cff
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-01 16:53:32 +0300

    Add CategoryMapping

---
+++ BlueBerry24.Application/Mapping/CategoryMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.CategoryDtos;
+using BlueBerry24.Domain.Entities.Product;
+
+namespace BlueBerry24.Application.Mapping
+{
+    class CategoryMapping : Profile
+    {
+        public CategoryMapping()
+        {
+            CreateMap<CategoryDto, CreateCategoryDto>().ReverseMap();
+            CreateMap<CategoryDto, UpdateCategoryDto>().ReverseMap();
+            CreateMap<CategoryDto, DeleteCategoryDto>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap();
+        }
+    }
+}
