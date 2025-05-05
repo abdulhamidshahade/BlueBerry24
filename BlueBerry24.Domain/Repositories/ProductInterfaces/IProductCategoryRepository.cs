3cdc010258bda701adfaee172b210aaf9465b01c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 02:50:13 +0300

    Add contracts to IProductCategoryRepository = Task<List<Category>> GetCategoriesByProuductId(int productId) - Task<bool> RemoveCategoriesByProductId(int productId)

--- BlueBerry24.Domain/Repositories/ProductInterfaces/IProductCategoryRepository.cs
+++ BlueBerry24.Domain/Repositories/ProductInterfaces/IProductCategoryRepository.cs
@@ -6,5 +6,7 @@ namespace BlueBerry24.Domain.Repositories.ProductInterfaces
     {
         Task<bool> AddProductCategoryAsync(Product Product, List<int> categories);
         Task<bool> UpdateProductCategoryAsync(Product product, List<int> categories);
+        Task<List<Category>> GetCategoriesByProuductId(int productId);
+        Task<bool> RemoveCategoriesByProductId(int productId);
     }
 }
