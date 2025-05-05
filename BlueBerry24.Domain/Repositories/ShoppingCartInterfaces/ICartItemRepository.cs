1adaf1f76a6689b992280e0e50fdfb79a8b85ebe
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 02:57:16 +0300

    Update ICartItemRepository with contracts - Task<bool> LoadItemsToPersistenceStorage(List<CartItem> items, int headerId) - Task<bool> RemoveItemsFromPersistenceStorage(int headerId)

--- BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartItemRepository.cs
+++ BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartItemRepository.cs
@@ -9,12 +9,19 @@ namespace BlueBerry24.Domain.Repositories.ShoppingCartInterfaces
 {
     public interface ICartItemRepository
     {
-        Task<CartItem> AddItemAsync(CartItem item);
-        Task<bool> RemoveItemAsync(CartItem item);
-        Task<CartItem> UpdateItemCountAsync(CartItem item, int newCount);
+        //Task<CartItem> AddItemAsync(CartItem item);
+        //Task<bool> RemoveItemAsync(CartItem item);
+        //Task<CartItem> UpdateItemCountAsync(CartItem item, int newCount);
 
-        Task<bool> ExistsByUserIdAsync(int userId, int headerId);
-        Task<bool> IncreaseItemAsync(CartItem item);
-        Task<bool> DecreaseItemAsync(CartItem item);
+
+        //Task<bool> IncreaseItemAsync(CartItem item);
+        //Task<bool> DecreaseItemAsync(CartItem item);
+
+
+        //Task<bool> ExistsByUserIdAsync(int userId, int headerId);
+
+
+        Task<bool> LoadItemsToPersistenceStorage(List<CartItem> items, int headerId);
+        Task<bool> RemoveItemsFromPersistenceStorage(int headerId);
     }
 }
