81066ea9a65f34ba73a2affbc73c82dae2ebd47c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 14:13:26 +0300

    Add ICartHeaderService interface

---
+++ BlueBerry24.Application/Services/Interfaces/ShoppingCartServiceInterfaces/ICartHeaderService.cs
@@ -0,0 +1,18 @@
+﻿using BlueBerry24.Application.Dtos.ShoppingCartDtos;
+using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ShoppingCartServiceInterfaces
+{
+    interface ICartHeaderService
+    {
+        Task<bool> ExistsByIdAsync(int id);
+        Task<CartHeaderDto> CreateCartAsync(CartHeaderDto header);
+        Task<bool> DeleteCartHeaderAsync(int id);
+        Task<CartHeaderDto> UpdateCartHeaderAsync(int id, CartHeaderDto header);
+    }
+}
