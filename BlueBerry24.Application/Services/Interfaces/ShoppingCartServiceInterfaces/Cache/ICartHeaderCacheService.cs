92c6baea6b8ce18da8215b52f853aec400f0162b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 14:12:37 +0300

    Add ICartHeaderCacheService interface

---
+++ BlueBerry24.Application/Services/Interfaces/ShoppingCartServiceInterfaces/Cache/ICartHeaderCacheService.cs
@@ -0,0 +1,19 @@
+﻿using BlueBerry24.Application.Dtos.ShoppingCartDtos;
+using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ShoppingCartServiceInterfaces.Cache
+{
+    public interface ICartHeaderCacheService
+    {
+        Task<bool> ExistsByUserIdAsync(int userId);
+        Task<bool> CreateCartHeaderAsync(int userId, TimeSpan timeSpan);
+        Task<bool> DeleteCartHeaderAsync(int userId);
+        Task<bool> UpdateCartHeaderAsync(int userId, CartHeaderDto cartHeader);
+        Task<CartHeaderDto> GetCartHeaderAsync(int userId);
+    }
+}
