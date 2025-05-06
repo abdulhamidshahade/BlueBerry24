8c0230b2c67ae695e9c36b2c6fc063968c37750e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 14:11:00 +0300

    Add ProductCategoryService concrete class

---
+++ BlueBerry24.Application/Services/Concretes/ProductServiceConcretes/ProductCategoryService.cs
@@ -0,0 +1,68 @@
+﻿
+using AutoMapper;
+using BlueBerry24.Application.Dtos.ProductDtos;
+using BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces;
+using BlueBerry24.Domain.Entities.Product;
+using BlueBerry24.Domain.Repositories;
+using BlueBerry24.Domain.Repositories.ProductInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlueBerry24.Application.Services.Concretes.ProductServiceConcretes
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductCategoryService(IProductCategoryRepository productCategoryRepository, 
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            _productCategoryRepository = productCategoryRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> AddProductCategoryAsync(ProductDto product, List<int> categories)
+        {
+            if(categories.Count == 0 || !await _productRepository.ExistsByIdAsync(product.Id))
+            {
+                return false;
+            }
+
+            var mappedProduct = _mapper.Map<Product>(product);
+
+            var created = await _productCategoryRepository.AddProductCategoryAsync(mappedProduct, categories);
+
+            return created;
+        }
+
+        public async Task<bool> UpdateProductCategoryAsync(ProductDto product, List<int> categories)
+        {
+            if (categories.Count == 0 || !await _productRepository.ExistsByIdAsync(product.Id))
+            {
+                return false;
+            }
+
+            await _unitOfWork.BeginTransactionAsync();
+
+            var existingCategories = await _productCategoryRepository.GetCategoriesByProuductId(product.Id);
+            
+            if(!await _productCategoryRepository.RemoveCategoriesByProductId(product.Id))
+            {
+                await _unitOfWork.RollbackTransactionAsync();
+                return false;
+            }
+
+            await _unitOfWork.CommitTransactionAsync();
+
+            var mappedProduct = _mapper.Map<Product>(product);
+
+            bool result = await _productCategoryRepository.AddProductCategoryAsync(mappedProduct, categories);
+
+            return result;
+        }
+    }
+}
