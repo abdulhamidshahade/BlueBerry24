68a7a38046fc0fa6caf242012703222c1154dc4b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-06 14:07:23 +0300

    Implement UnitOfWork concrete class

--- BlueBerry24.Infrastructure/Repositories/UnitOfWork.cs
+++ BlueBerry24.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using BlueBerry24.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore.Storage;
+using StackExchange.Redis;
 
 namespace BlueBerry24.Domain.Repositories
 {
@@ -7,22 +8,62 @@ namespace BlueBerry24.Domain.Repositories
     {
         private readonly ApplicationDbContext _context;
         private IDbContextTransaction _currentTransaction;
-        public UnitOfWork(ApplicationDbContext context, IDbContextTransaction currentTransaction)
+        private ITransaction _cacheTransaction;
+        private readonly StackExchange.Redis.IDatabase _db;
+        public UnitOfWork(ApplicationDbContext context, 
+                          IDbContextTransaction currentTransaction, 
+                          ITransaction cacheTransaction,
+                          IConnectionMultiplexer redis)
         {
             _context = context;
             _currentTransaction = currentTransaction;
+            _cacheTransaction = cacheTransaction;
+            _db = redis.GetDatabase();
         }
+
+
+        public void BeginCacheTransaction()
+        {
+            _cacheTransaction = _db.CreateTransaction();
+        }
+
+        public async Task<bool> CacheCommitTransactionAsync()
+        {
+            if(_cacheTransaction == null)
+            {
+                return false;
+            }
+
+            return await _cacheTransaction.ExecuteAsync();
+        }
+
+
+        public async Task ExecuteInTransactionCacheAsync(Func<ITransaction, Task> action)
+        {
+            if(_cacheTransaction == null)
+            {
+                BeginCacheTransaction();
+            }
+
+            await action(_cacheTransaction!);
+        }
+
+
 
         public async Task BeginTransactionAsync()
         {
             _currentTransaction = await _context.Database.BeginTransactionAsync();
         }
 
+        
+
         public async Task CommitTransactionAsync()
         {
             await _currentTransaction.CommitAsync();
             await _currentTransaction.DisposeAsync();
         }
+
+        
 
         public async Task RollbackTransactionAsync()
         {
