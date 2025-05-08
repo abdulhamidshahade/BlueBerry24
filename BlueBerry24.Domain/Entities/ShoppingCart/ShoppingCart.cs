4596daa8780c0d9d69c16e2f77f2f927f3ca3793
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-08 23:56:37 +0300

    Add ShoppingCart domain entity

---
+++ BlueBerry24.Domain/Entities/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeader CartHeader { get; set; }
+        public List<CartItem> CartItems { get; set; }
+        public bool IsActive { get; set; }
+        public int UserId { get; set; }
+    }
+}
