5b53a09cd0cf84834e19c7f99af48fbdb73cf494
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-09 00:15:01 +0300

    Update the access level of IAuthService interface

--- BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
@@ -2,7 +2,7 @@
 
 namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
 {
-    interface IAuthService
+    public interface IAuthService
     {
         Task<RegisterResponseDto> Register(RegisterRequestDto requestDto);
         Task<LoginResponseDto> Login(LoginRequestDto requestDto);
