811a7d95d66b50cd282c760fa8a5f8cd18f6245d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-08 23:56:09 +0300

    Add ICartRepository interface

---
+++ BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories.ShoppingCartInterfaces
+{
+    public interface ICartRepository
+    {
+        Task<ShoppingCart> UpdateCartAsync(int userId, bool isActive);
+        Task<ShoppingCart> CreateCartAsync(int userId, int headerId);
+        Task<ShoppingCart> GetCartAsync(int userId);
+        Task<List<ShoppingCart>> GetCartsAsync();
+        Task<bool> DeleteCartAsync(int userId);
+
+    }
+}
