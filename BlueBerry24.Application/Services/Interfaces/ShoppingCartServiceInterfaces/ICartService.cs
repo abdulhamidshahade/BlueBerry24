b44ff0e513c741e966399f5cc4bfcb935ee227ed
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-08 23:55:17 +0300

    Add ICartService interface

---
+++ BlueBerry24.Application/Services/Interfaces/ShoppingCartServiceInterfaces/ICartService.cs
@@ -0,0 +1,20 @@
+﻿using BlueBerry24.Application.Dtos.ShoppingCartDtos;
+using BlueBerry24.Domain.Entities.ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ShoppingCartServiceInterfaces
+{
+    public interface ICartService
+    {
+        Task<CartDto> GetCartAsync(int userId);
+        Task<bool> DeleteCartAsync(int userId);
+        Task<CartDto> UpdateCartAsync(int userId, bool isActive);
+        Task<List<CartDto>> GetCartsAsync();
+        Task<bool> IsCartAsync(int userId);
+        Task<CartDto> CreateCartAsync(int userId, int headerId);
+    }
+}
