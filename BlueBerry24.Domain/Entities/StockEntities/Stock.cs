74f84e2f44089ee9b950982d1ca638fc0e998e1e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-09 17:23:33 +0300

    Refactor stock entities

---
+++ BlueBerry24.Domain/Entities/StockEntities/Stock.cs
@@ -0,0 +1,15 @@
+﻿using BlueBerry24.Domain.Entities.Base;
+
+namespace BlueBerry24.Domain.Entities.StockEntities
+{
+    public class Stock : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
