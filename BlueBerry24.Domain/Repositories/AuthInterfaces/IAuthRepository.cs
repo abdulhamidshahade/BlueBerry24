0b91a4763d8f2f8493773171055f96f904765d55
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-19 16:13:26 +0300

    Remove IAuthRepository and IUserRepository interfaces

--- BlueBerry24.Domain/Repositories/AuthInterfaces/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlueBerry24.Domain.Entities.Auth;
-
-namespace BlueBerry24.Domain.Repositories.AuthInterfaces
-{
-    public interface IAuthRepository
-    {
-        Task<ApplicationUser> Register(ApplicationUser requestModel);
-    }
-}
--- BlueBerry24.Domain/Repositories/AuthInterfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlueBerry24.Domain.Repositories.AuthInterfaces
-{
-    public interface IUserRepository
-    {
-        Task<bool> IsUserExistsByIdAsync(int userId);
-        Task<bool> IsUserExistsByEmailAsync(string emailAddress);
-    }
-}
