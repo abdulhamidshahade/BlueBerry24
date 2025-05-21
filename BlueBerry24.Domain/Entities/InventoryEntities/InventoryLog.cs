27f23ffd6bbba14e1956a6e0cdd9418c42f70906
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-21 23:45:42 +0300

    Add InventoryLog entity

---
+++ BlueBerry24.Domain/Entities/InventoryEntities/InventoryLog.cs
@@ -0,0 +1,46 @@
+﻿using BlueBerry24.Domain.Entities.Base;
+using BlueBerry24.Domain.Entities.ProductEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.InventoryEntities
+{
+    public class InventoryLog : IAuditableEntity
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public int CurrentStockQuantity { get; set; }
+
+        public int QuantityChanged { get; set; }
+
+        public InventoryChangeType ChangeType { get; set; }
+
+        public int ReferenceId { get; set; }
+        public string ReferenceType { get; set; }
+
+        public int? PerformedByUserId { get; set; }
+
+        public string Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public enum InventoryChangeType
+    {
+        Purchase,         
+        Return,           
+        StockAdjustment,  
+        InitialStock,      
+        Reserved,         
+        ReleaseReservation,
+        Damaged,          
+        Restock         
+    }
+}
