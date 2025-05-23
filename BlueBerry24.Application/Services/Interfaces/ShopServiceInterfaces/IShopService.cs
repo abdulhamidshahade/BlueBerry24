2d301f498b43200ee97a9cb4c0677e2547b038b5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-23 16:55:52 +0300

    Add IShopService interface with shop methods

---
+++ BlueBerry24.Application/Services/Interfaces/ShopServiceInterfaces/IShopService.cs
@@ -0,0 +1,16 @@
+﻿using BlueBerry24.Application.Dtos.ShopDtos;
+using BlueBerry24.Domain.Entities.ShopEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.ShopServiceInterfaces
+{
+    public interface IShopService
+    {
+        Task<Shop> GetShopAsync(int id);
+        Task<ShopDto> UpdateShopAsync(int id, UpdateShopDto shop);
+    }
+}
