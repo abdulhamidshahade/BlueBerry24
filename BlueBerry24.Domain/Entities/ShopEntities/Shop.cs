8aa0194631dfa414bfc4aefa61eeead09a626c04
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-23 16:58:52 +0300

    Add shop domain entity

---
+++ BlueBerry24.Domain/Entities/ShopEntities/Shop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.ShopEntities
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string Description { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public string Currency { get; set; }
+        public string Language { get; set; }
+    }
+}
