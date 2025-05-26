0f8f1b375c5763e3fdb6f5714263e27d364a3e38
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-26 23:14:54 +0300

    Add CartCouponDto class for shopping cart functionality

---
+++ BlueBerry24.Application/Dtos/ShoppingCartDtos/CartCouponDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.ShoppingCartDtos
+{
+    public class CartCouponDto
+    {
+        public int Id { get; set; }
+        public int CouponId { get; set; }
+        public int UserId { get; set; }
+        public int CartId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal DiscountAmount { get; set; }
+        public DateTime AppliedAt { get; set; }
+    }
+}
