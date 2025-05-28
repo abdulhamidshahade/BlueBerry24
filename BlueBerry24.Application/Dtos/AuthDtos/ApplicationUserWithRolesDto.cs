9df6dea1912e70207495d34aa69cf7d2545f4efb
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-28 15:19:05 +0300

    Add ApplicationUserWithRolesDto class for user data and his/her roles

---
+++ BlueBerry24.Application/Dtos/AuthDtos/ApplicationUserWithRolesDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class ApplicationUserWithRolesDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+        public bool EmailConfirmed { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public int AccessFailedCount { get; set; }
+    }
+}
