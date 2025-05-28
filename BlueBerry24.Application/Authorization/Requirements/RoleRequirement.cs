df227a72f7c5c9f0deb048de6725e4c78e842dec
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-28 21:15:08 +0300

    Add RoleRequirement class for role-based authorization

---
+++ BlueBerry24.Application/Authorization/Requirements/RoleRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlueBerry24.Application.Authorization.Requirements
+{
+    public class RoleRequirement : IAuthorizationRequirement
+    {
+        public string[] AllowedRoles { get; }
+
+        public RoleRequirement(params string[] allowedRoles)
+        {
+            AllowedRoles = allowedRoles ?? throw new ArgumentNullException(nameof(allowedRoles));
+        }
+    }
+}
