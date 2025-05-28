1df84e7e5ec8a2862995059e504b55c0fab6fb70
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-28 15:19:21 +0300

    Add RoleStatsDto class for role and user statistics

---
+++ BlueBerry24.Application/Dtos/AuthDtos/RoleStatsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class RoleStatsDto
+    {
+        public int TotalRoles { get; set; }
+        public int TotalUsers { get; set; }
+        public int UsersWithRoles { get; set; }
+        public int UsersWithoutRoles { get; set; }
+    }
+}
