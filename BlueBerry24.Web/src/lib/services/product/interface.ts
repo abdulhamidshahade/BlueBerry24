3591d16efeaf87b45749a2b23c59819b1761650d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-28 20:59:37 +0300

    Add IProductService interface for product management

---
+++ BlueBerry24.Web/src/lib/services/product/interface.ts
@@ -0,0 +1,12 @@
+import { CreateProductDto, ProductDto, UpdateProductDto } from "@/types/product";
+
+export interface IProductService{
+    getById(id: number): Promise<ProductDto>;
+    getByName(name: string): Promise<ProductDto>;
+    getAll(): Promise<ProductDto[]>;
+    create(data: CreateProductDto, categories: number[]): Promise<ProductDto>;
+    update(id: number, data: UpdateProductDto, categories: number[]): Promise<ProductDto>;
+    delete(id: number): Promise<boolean>;
+    existsById(id: number): Promise<boolean>;
+    existsByName(name: string): Promise<boolean>;
+}
