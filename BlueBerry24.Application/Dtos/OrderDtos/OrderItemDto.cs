8561af12c793f4337ba92da0d22f26292d6e0ca6
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-28 15:20:21 +0300

    Add OrderItemDto class for order item representation

---
+++ BlueBerry24.Application/Dtos/OrderDtos/OrderItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.OrderDtos
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string? ProductDescription { get; set; }
+        public string? ProductSKU { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
