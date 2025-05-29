041820ee003ab2fff85917cbdcddbf548b2c12d5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-29 13:13:21 +0300

    Add Products property to CategoryDto

--- BlueBerry24.Application/Dtos/CategoryDtos/CategoryDto.cs
+++ BlueBerry24.Application/Dtos/CategoryDtos/CategoryDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlueBerry24.Application.Dtos.ProductDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,7 @@ namespace BlueBerry24.Application.Dtos.CategoryDtos
     public class CategoryDto : CategoryBaseDto
     {
         public int Id { get; set; }
+
+        public List<ProductDto> Products { get; set; }
     }
 }
