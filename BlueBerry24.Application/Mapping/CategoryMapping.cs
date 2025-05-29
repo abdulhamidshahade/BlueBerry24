88cb9ac3cab462595a65b1cee1b4baada9592af5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-30 00:05:06 +0300

    Refactor CategoryMapping class

--- BlueBerry24.Application/Mapping/CategoryMapping.cs
+++ BlueBerry24.Application/Mapping/CategoryMapping.cs
@@ -9,11 +9,7 @@ namespace BlueBerry24.Application.Mapping
         public CategoryMapping()
         {
             CreateMap<CategoryDto, CreateCategoryDto>().ReverseMap();
-            CreateMap<CategoryDto, UpdateCategoryDto>().ReverseMap();
-            CreateMap<CategoryDto, DeleteCategoryDto>().ReverseMap();
-            
-
-
+            CreateMap<CategoryDto, UpdateCategoryDto>().ReverseMap()
             CreateMap<Category, CreateCategoryDto>().ReverseMap();
             CreateMap<Category, UpdateCategoryDto>().ReverseMap();
 
