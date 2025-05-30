af4485f5e30c0e6663aedadfa3ab9a598d4eef04
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-30 12:26:27 +0300

    Refactor Coupon domain entity

--- BlueBerry24.Domain/Entities/CouponEntities/Coupon.cs
+++ BlueBerry24.Domain/Entities/CouponEntities/Coupon.cs
@@ -1,4 +1,6 @@
-﻿using BlueBerry24.Domain.Entities.Base;
+﻿using BlueBerry24.Domain.Constants;
+using BlueBerry24.Domain.Entities.Base;
+using BlueBerry24.Domain.Entities.ShoppingCartEntities;
 
 namespace BlueBerry24.Domain.Entities.CouponEntities
 {
@@ -6,12 +8,21 @@ namespace BlueBerry24.Domain.Entities.CouponEntities
     {
         public int Id { get; set; }
         public string Code { get; set; }
+        public string Description { get; set; } = string.Empty;
         public decimal DiscountAmount { get; set; }
-        public decimal MinimumAmount { get; set; }
+        public decimal? MinimumOrderAmount { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
+        public CouponType Type { get; set; }
+
+        public decimal Value { get; set; }
+
+        public bool IsForNewUsersOnly { get; set; }
+
         public List<UserCoupon> UserCoupons { get; set; }
+
+        public List<CartCoupon> CartCoupons { get; set; }
     }
 }
