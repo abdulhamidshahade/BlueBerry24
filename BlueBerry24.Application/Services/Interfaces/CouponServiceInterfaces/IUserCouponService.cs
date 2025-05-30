2f9b0b631290bfe72344b617c52477a00168c4ec
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-30 12:25:34 +0300

    Update IUserCouponService methods with CouponDto and ApplicationUserDto return type

--- BlueBerry24.Application/Services/Interfaces/CouponServiceInterfaces/IUserCouponService.cs
+++ BlueBerry24.Application/Services/Interfaces/CouponServiceInterfaces/IUserCouponService.cs
@@ -1,4 +1,5 @@
-﻿using BlueBerry24.Application.Dtos.CouponDtos;
+﻿using BlueBerry24.Application.Dtos.AuthDtos;
+using BlueBerry24.Application.Dtos.CouponDtos;
 
 namespace BlueBerry24.Application.Services.Interfaces.CouponServiceInterfaces
 {
@@ -6,8 +7,8 @@ namespace BlueBerry24.Application.Services.Interfaces.CouponServiceInterfaces
     {
         Task<UserCouponDto> AddCouponToUserAsync(int userId, int couponId);
         Task<bool> DisableCouponToUser(int usreId, int couponId);
-        Task<List<string>> GetCouponsByUserIdAsync(int userId);
-        Task<List<string>> GetUsersByCouponIdAsync(int couponId);
-        Task<bool> IsCouponUsedByUser(int userId, int couponId);
+        Task<List<CouponDto>> GetCouponsByUserIdAsync(int userId);
+        Task<List<ApplicationUserDto>> GetUsersByCouponIdAsync(int couponId);
+        Task<bool> IsCouponUsedByUser(int userId, string couponCode);
     }
 }
