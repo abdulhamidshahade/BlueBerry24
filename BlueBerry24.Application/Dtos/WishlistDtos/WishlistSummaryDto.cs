3bdcc661ffb4382edc5c011cc897104f10fb0f7e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:46:28 +0300

    Add WishlistSummaryDto class for wishlist management

---
+++ BlueBerry24.Application/Dtos/WishlistDtos/WishlistSummaryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Application.Dtos.WishlistDtos
+{
+    public class WishlistSummaryDto
+    {
+        public int TotalWishlists { get; set; }
+        public int TotalItems { get; set; }
+        public decimal TotalValue { get; set; }
+        public List<WishlistDto> RecentWishlists { get; set; } = new List<WishlistDto>();
+    }
+}
