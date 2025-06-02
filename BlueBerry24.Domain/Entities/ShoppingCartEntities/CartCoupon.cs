41efd62449a4d56c34429e648bf2c8921b3a1b08
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:16:29 +0300

    Add CartCoupon class for shopping cart functionality

---
+++ BlueBerry24.Domain/Entities/ShoppingCartEntities/CartCoupon.cs
@@ -0,0 +1,42 @@
+﻿using BlueBerry24.Domain.Entities.AuthEntities;
+using BlueBerry24.Domain.Entities.Base;
+using BlueBerry24.Domain.Entities.CouponEntities;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.ShoppingCartEntities
+{
+    public class CartCoupon : IAuditableEntity
+    {
+        public int Id { get; set; }
+
+
+        public int CartId { get; set; }
+        public int CouponId { get; set; }
+
+
+        public int? UserId { get; set; }
+        public string? SessionId { get; set; }
+
+
+        public decimal DiscountAmount { get; set; }
+
+
+        public DateTime AppliedAt { get; set; }
+
+
+        public virtual Cart Cart { get; set; } = null!;
+        public virtual Coupon Coupon { get; set; } = null!;
+        public virtual ApplicationUser? User { get; set; }
+
+
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
