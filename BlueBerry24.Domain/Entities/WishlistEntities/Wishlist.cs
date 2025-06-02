580d4296fb822727f358c5dec1e50c6adc13a599
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:12:08 +0300

    Add Wishlist domain entity to manage user wishlists

---
+++ BlueBerry24.Domain/Entities/WishlistEntities/Wishlist.cs
@@ -0,0 +1,34 @@
+﻿using BlueBerry24.Domain.Entities.AuthEntities;
+using BlueBerry24.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.WishlistEntities
+{
+    public class Wishlist : IAuditableEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+
+        public string? Name { get; set; } = "My Wishlist";
+
+        public bool IsDefault { get; set; } = true;
+
+        public bool IsPublic { get; set; } = false;
+
+        public ApplicationUser User { get; set; }
+        public ICollection<WishlistItem> WishlistItems { get; set; } = new List<WishlistItem>();
+
+        public int ItemCount => WishlistItems?.Count ?? 0;
+        public decimal TotalValue => WishlistItems?.Sum(x => x.Product?.Price ?? 0) ?? 0;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
