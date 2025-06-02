418498e32e971e085873ac0cfb94ad1c2c6ae3ca
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:40:40 +0300

    Refactor CartDto with new properties and updates

--- BlueBerry24.Application/Dtos/ShoppingCartDtos/CartDto.cs
+++ BlueBerry24.Application/Dtos/ShoppingCartDtos/CartDto.cs
@@ -1,11 +1,24 @@
-﻿namespace BlueBerry24.Application.Dtos.ShoppingCartDtos
+﻿using BlueBerry24.Domain.Constants;
+
+namespace BlueBerry24.Application.Dtos.ShoppingCartDtos
 {
     public class CartDto
     {
-        public int CartHeaderId { get; set; }
+        public int Id { get; set; }
         public bool IsActive { get; set; }
-        public int UserId { get; set; }
-        public CartHeaderDto ShoppingCartHeaderDto { get; set; }
-        public List<CartItemDto> CartItems { get; set; }
+        public int? UserId { get; set; }
+        public string? SessionId { get; set; }
+        public CartStatus Status { get; set; }
+
+        public DateTime? ExpiresAt { get; set; }
+
+        public List<CartItemDto> CartItems { get; set; } = new List<CartItemDto>();
+        public List<CartCouponDto> CartCoupons { get; set; } = new List<CartCouponDto>();
+
+        public string? Note { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal DiscountTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal Total { get; set; }
     }
 }
