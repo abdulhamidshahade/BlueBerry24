c152418032dd0bce15aa3f470ddff5e2f5593d8f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:51:05 +0300

    Add OrderTotal class with financial properties

---
+++ BlueBerry24.Domain/Entities/OrderEntities/OrderTotal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.OrderEntities
+{
+    public class OrderTotal
+    {
+        public decimal SubTotal { get; set; }
+        public decimal DiscountTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal ShippingAmount { get; set; }
+        public decimal Total { get; set; }
+    }
+}
