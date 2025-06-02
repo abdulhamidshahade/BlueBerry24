d9addf988b8c9ce214c3dbaf3acc94bf2e23f9f0
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:37:05 +0300

    Add BulkAssignmentResultDto class for assignment results

---
+++ BlueBerry24.Application/Dtos/AuthDtos/BulkAssignmentResultDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class BulkAssignmentResultDto
+    {
+        public int TotalUsers { get; set; }
+        public int SuccessfulAssignments { get; set; }
+        public int FailedAssignments { get; set; }
+        public List<string> FailedUserIds { get; set; } = new List<string>();
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public bool IsSuccess => FailedAssignments == 0;
+    }
+}
