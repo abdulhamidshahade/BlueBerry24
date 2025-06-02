9e443f9b3f18182e75245d2f82951473306cbd75
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:13:45 +0300

    Add WishlistItem class for managing wishlist entries

---
+++ BlueBerry24.Domain/Entities/WishlistEntities/WishlistItem.cs
@@ -0,0 +1,32 @@
+﻿using BlueBerry24.Domain.Entities.Base;
+using BlueBerry24.Domain.Entities.ProductEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Entities.WishlistEntities
+{
+    public class WishlistItem : IAuditableEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int WishlistId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public string? Notes { get; set; }
+
+        public int Priority { get; set; } = 1; // 1=Low, 2=Medium, 3=High
+
+        // Navigation properties
+        public Wishlist Wishlist { get; set; }
+        public Product Product { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
