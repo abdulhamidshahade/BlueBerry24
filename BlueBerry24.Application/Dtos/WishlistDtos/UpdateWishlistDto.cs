4a3b68aa2a38e2c2167c90b05aacef381000da33
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:45:33 +0300

    Add UpdateWishlistDto class for wishlist updates

---
+++ BlueBerry24.Application/Dtos/WishlistDtos/UpdateWishlistDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueBerry24.Application.Dtos.WishlistDtos
+{
+    public class UpdateWishlistDto
+    {
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
