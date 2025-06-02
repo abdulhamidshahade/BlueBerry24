88a8df0b764705c4fbfee72de1a75e4a0e42758b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 22:57:53 +0300

    Add ApplicationRoleDto class with properties

---
+++ BlueBerry24.Application/Dtos/AuthDtos/ApplicationRoleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class ApplicationRoleDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string concurrencyStamp { get; set; }
+        public string normalizedName { get; set; }
+    }
+}
