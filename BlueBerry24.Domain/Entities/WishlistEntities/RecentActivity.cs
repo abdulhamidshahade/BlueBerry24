4894e925c1a716381b31cb7e2923f8a263bb98e5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:13:17 +0300

    Add RecentActivity class for wishlist tracking

---
+++ BlueBerry24.Domain/Entities/WishlistEntities/RecentActivity.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.WishlistDtos
+{
+
+    public class RecentActivity
+    {
+        public string Date { get; set; }
+        public int NewWishlists { get; set; }
+        public int NewItems { get; set; }
+    }
+}
