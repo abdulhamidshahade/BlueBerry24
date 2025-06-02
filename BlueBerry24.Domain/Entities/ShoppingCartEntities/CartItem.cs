dabcea4038b5334c7e58ac605bdc1105d3f902cb
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:16:07 +0300

    Refactor CartItem class for improve structure

--- BlueBerry24.Domain/Entities/ShoppingCartEntities/CartItem.cs
+++ BlueBerry24.Domain/Entities/ShoppingCartEntities/CartItem.cs
@@ -1,27 +1,23 @@
 ﻿using BlueBerry24.Domain.Entities.Base;
+using BlueBerry24.Domain.Entities.ProductEntities;
 using System.ComponentModel.DataAnnotations;
-
 
 namespace BlueBerry24.Domain.Entities.ShoppingCartEntities
 {
     public class CartItem : IAuditableEntity
     {
         public int Id { get; set; }
-        public int ShoppingCartId { get; set; }
-        public ShoppingCart ShoppingCart { get; set; }
-        public int ProductId { get; set; }
-
-        //[NotMapped]
-        //public ProductDto Product { get; set; }
-        public int Count { get; set; }
-
-
-        //[Timestamp]
-        //public byte[] RowVersion { get; set; }
-
-
+        public int? ShoppingCartId { get; set; }
+        public int? UserId { get; set; }
+        public string? SessionId { get; set; }
+        public Cart ShoppingCart { get; set; }
+        public int ProductId { get; set; }  
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        public decimal UnitPrice { get; set; }
 
     }
 }
