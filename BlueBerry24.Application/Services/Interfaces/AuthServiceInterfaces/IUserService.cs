c1992bc86a8bdc07d792f47a27b68798a74c2805
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:46:50 +0300

    Update IUserService with user management methods

--- BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IUserService.cs
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IUserService.cs
@@ -1,8 +1,19 @@
-﻿namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
+﻿using BlueBerry24.Application.Dtos.AuthDtos;
+
+namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
 {
     public interface IUserService
     {
         Task<bool> IsUserExistsByIdAsync(int userId);
         Task<bool> IsUserExistsByEmailAsync(string emailAddress);
+        Task<List<ApplicationUserDto>> GetAllUsers();
+        Task<ApplicationUserDto> GetUserById(int id);
+        Task<bool> LockUserAccountAsync(int userId, DateTime? lockoutEnd = null);
+        Task<bool> UnlockUserAccountAsync(int userId);
+        Task<bool> ResetUserPasswordAsync(int userId, string newPassword);
+        Task<bool> VerifyUserEmailAsync(int userId);
+        Task<bool> UpdateUserAsync(int userId, UpdateUserDto updateUserDto);
+        Task<ApplicationUserDto> CreateUserAsync(CreateUserDto createUserDto);
+        Task<bool> DeleteUserAsync(int userId);
     }
 }
