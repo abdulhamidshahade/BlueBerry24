1e7dd304a863671987208ad0dcb06dd46c964aa7
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:33:09 +0300

    Add IWishlistRepository interface

---
+++ BlueBerry24.Domain/Repositories/WishlistInterfaces/IWishlistRepository.cs
@@ -0,0 +1,35 @@
+﻿using BlueBerry24.Domain.Entities.WishlistEntities;
+using BlueBerry24.Application.Dtos.WishlistDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Repositories.WishlistInterfaces
+{
+    public interface IWishlistRepository
+    {
+        Task<Wishlist> GetByIdAsync(int id);
+        Task<Wishlist> GetUserDefaultWishlistAsync(int userId);
+        Task<IEnumerable<Wishlist>> GetUserWishlistsAsync(int userId);
+        Task<Wishlist> CreateAsync(Wishlist wishlist);
+        Task<Wishlist> UpdateAsync(Wishlist wishlist);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+
+        Task<WishlistItem> GetWishlistItemAsync(int wishlistId, int productId);
+        Task<WishlistItem> AddItemAsync(WishlistItem item);
+        Task<WishlistItem> UpdateItemAsync(WishlistItem item);
+        Task<bool> RemoveItemAsync(int wishlistId, int productId);
+        Task<bool> IsProductInWishlistAsync(int userId, int productId);
+        Task<IEnumerable<WishlistItem>> GetWishlistItemsAsync(int wishlistId);
+
+        Task<int> GetUserWishlistCountAsync(int userId);
+        Task<int> GetUserTotalItemsAsync(int userId);
+        Task<decimal> GetUserTotalValueAsync(int userId);
+
+        Task<IEnumerable<Wishlist>> GetAllWishlistsAsync();
+        Task<GlobalWishlistStats> GetGlobalStatsAsync();
+    }
+}
