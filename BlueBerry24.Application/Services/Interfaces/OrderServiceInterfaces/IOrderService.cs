18a7e309796c69df3f0f2324d43506383ebfbd65
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:52:07 +0300

    Refactor IOrderService to use DTOs for orders

--- BlueBerry24.Application/Services/Interfaces/OrderServiceInterfaces/IOrderService.cs
+++ BlueBerry24.Application/Services/Interfaces/OrderServiceInterfaces/IOrderService.cs
@@ -1,4 +1,5 @@
 ﻿using BlueBerry24.Application.Dtos.OrderDtos;
+using BlueBerry24.Domain.Constants;
 using BlueBerry24.Domain.Entities.OrderEntities;
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,16 @@ namespace BlueBerry24.Application.Services.Interfaces.OrderServiceInterfaces
 {
     public interface IOrderService
     {
-        Task<Order?> GetOrderByIdAsync(int orderId);
+        Task<OrderDto?> GetOrderByIdAsync(int orderId);
 
-        Task<List<Order>> GetUserOrdersAsync(int userId, int page = 1, int pageSize = 10);
+        Task<List<OrderDto>> GetUserOrdersAsync(string sessionId, int page = 1, int pageSize = 10);
+        Task<List<OrderDto>> GetAllOrdersAsync(int page = 1, int pageSize = 50);
         Task<Order?> CreateOrderFromCartAsync(int cartId, CreateOrderDto orderDto);
         Task<bool> UpdateOrderStatusAsync(Order order, OrderStatus newStatus);
         Task<bool> MarkOrderAsPaidAsync(int orderId, int paymentTransactionId, string paymentProvider);
         Task<bool> CancelOrderAsync(int orderId, string reason);
         Task<bool> RefundOrderAsync(int orderId, string reason);
+        Task<bool> ProcessOrderAsync(int orderId);
         Task<OrderTotal> CalculateOrderTotalsAsync(int cartId);
         Task<string> GenerateUniqueReferenceNumberAsync();
         Task<List<Order>> GetOrdersByStatusAsync(OrderStatus status, int page = 1, int pageSize = 10);
