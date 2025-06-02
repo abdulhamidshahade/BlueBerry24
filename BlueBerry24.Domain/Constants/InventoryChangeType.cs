d7dbda84fdb527f9aa5a838c3c5eec53e1b4fd81
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:19:15 +0300

    Add InventoryChangeType enum to constants namespace

---
+++ BlueBerry24.Domain/Constants/InventoryChangeType.cs
@@ -0,0 +1,14 @@
+﻿namespace BlueBerry24.Domain.Constants
+{
+    public enum InventoryChangeType
+    {
+        Purchase,         
+        Return,           
+        StockAdjustment,  
+        InitialStock,      
+        Reserved,         
+        ReleaseReservation,
+        Damaged,          
+        Restock         
+    }
+}
