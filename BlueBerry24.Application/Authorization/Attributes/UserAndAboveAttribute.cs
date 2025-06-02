ac27c439a44e13ad3b240722667539a7c4e1a93f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:27:22 +0300

    Add UserAndAboveAttribute for role-based access control

---
+++ BlueBerry24.Application/Authorization/Attributes/UserAndAboveAttribute.cs
@@ -0,0 +1,9 @@
+﻿using BlueBerry24.Domain.Constants;
+
+namespace BlueBerry24.Application.Authorization.Attributes
+{
+    public class UserAndAboveAttribute : AuthorizeRolesAttribute
+    {
+        public UserAndAboveAttribute() : base(RoleConstants.SuperAdmin, RoleConstants.Admin, RoleConstants.User) { }
+    }
+}
