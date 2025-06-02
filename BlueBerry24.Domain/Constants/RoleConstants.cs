d7761c3f5c0c5080cb7237e04c9783fa7968bdc1
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:20:34 +0300

    Add RoleConstants class for user role management

---
+++ BlueBerry24.Domain/Constants/RoleConstants.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace BlueBerry24.Domain.Constants
+{
+    public class RoleConstants
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+        public static readonly string[] AllRoles = { SuperAdmin, Admin, User };
+
+        public static readonly string[] AdminRoles = { SuperAdmin, Admin };
+        public static readonly string[] UserAndAbove = { SuperAdmin, Admin, User };
+        public static readonly string[] AllIncludingGuest = { SuperAdmin, Admin, User };
+    }
+}
