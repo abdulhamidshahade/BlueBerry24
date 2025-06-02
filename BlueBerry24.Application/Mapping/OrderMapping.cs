ca10c2e870433b59ee63727ad7d15e89113b150a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:29:52 +0300

    Add OrderMapping class for AutoMapper configurations

---
+++ BlueBerry24.Application/Mapping/OrderMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.OrderDtos;
+using BlueBerry24.Domain.Entities.OrderEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    public class OrderMapping : Profile
+    {
+        public OrderMapping()
+        {
+            CreateMap<Order, OrderDto>()
+           .ReverseMap();
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ReverseMap();
+        }
+    }
+}
