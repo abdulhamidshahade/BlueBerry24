89676c6b7f3d2182493ffa21a02af2d30eb4554e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:04:42 +0300

    Add IRoleManagementService interface for role management

---
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IRoleManagementService.cs
@@ -0,0 +1,27 @@
+﻿using BlueBerry24.Application.Dtos.AuthDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
+{
+    public interface IRoleManagementService
+    {
+        Task<bool> CreateRoleAsync(string roleName);
+        Task<bool> DeleteRoleAsync(string roleName);
+        Task<bool> AssignRoleToUserAsync(int userId, string roleName);
+        Task<bool> RemoveRoleFromUserAsync(int userId, string roleName);
+        Task<List<string>> GetUserRolesAsync(int userId);
+        Task<List<ApplicationRoleDto>> GetAllRolesAsync();
+        Task<List<ApplicationUserDto>> GetUsersInRoleAsync(string roleName);
+        Task<bool> IsUserInRoleAsync(int userId, string roleName);
+        Task InitializeDefaultRolesAsync();
+        Task<List<ApplicationUserWithRolesDto>> GetAllUsersAsync();
+        Task<ApplicationUserWithRolesDto> GetUserByIdAsync(int userId);
+        Task<RoleStatsDto> GetRoleStatsAsync();
+        Task<bool> UpdateRoleAsync(string oldRoleName, string newRoleName);
+        Task<BulkAssignmentResultDto> BulkAssignRoleAsync(List<int> userIds, string roleName);
+    }
+}
