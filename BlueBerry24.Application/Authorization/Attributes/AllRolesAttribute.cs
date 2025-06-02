ccfb8307c0bdbb3ef09c82c291059953a5f68089
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-02 23:28:07 +0300

    Add AllRolesAttribute for role-based access control

---
+++ BlueBerry24.Application/Authorization/Attributes/AllRolesAttribute.cs
@@ -0,0 +1,9 @@
+﻿using BlueBerry24.Domain.Constants;
+
+namespace BlueBerry24.Application.Authorization.Attributes
+{
+    public class AllRolesAttribute : AuthorizeRolesAttribute
+    {
+        public AllRolesAttribute() : base(RoleConstants.SuperAdmin, RoleConstants.Admin, RoleConstants.User) { }
+    }
+}
