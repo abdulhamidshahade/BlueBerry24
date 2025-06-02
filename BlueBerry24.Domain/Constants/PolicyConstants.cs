4be2854739719b45162444bcc549e1ea5a7eabf5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:19:34 +0300

    Add PolicyConstants class for user roles and permissions

---
+++ BlueBerry24.Domain/Constants/PolicyConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Constants
+{
+    public static class PolicyConstants
+    {
+        public const string SuperAdminOnly = "SuperAdminOnly";
+        public const string AdminAndAbove = "AdminAndAbove";
+        public const string UserAndAbove = "UserAndAbove";
+        public const string AllRoles = "AllRoles";
+
+        public const string CanManageUsers = "CanManageUsers";
+        public const string CanManageProducts = "CanManageProducts";
+        public const string CanManageOrders = "CanManageOrders";
+        public const string CanManageCoupons = "CanManageCoupons";
+        public const string CanViewReports = "CanViewReports";
+        public const string CanManageShops = "CanManageShops";
+        public const string CanManageInventory = "CanManageInventory";
+    }
+}
