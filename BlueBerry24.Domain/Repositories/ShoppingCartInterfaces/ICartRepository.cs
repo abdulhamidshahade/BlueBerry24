aade3b619986bb269fe41a2de51d13f64b98fe00
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 00:21:10 +0300

    Refactor ICartRepository for enhance cart management

--- BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartRepository.cs
+++ BlueBerry24.Domain/Repositories/ShoppingCartInterfaces/ICartRepository.cs
@@ -1,4 +1,5 @@
-﻿using BlueBerry24.Domain.Entities.ShoppingCartEntities;
+﻿using BlueBerry24.Domain.Constants;
+using BlueBerry24.Domain.Entities.ShoppingCartEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,19 @@ namespace BlueBerry24.Domain.Repositories.ShoppingCartInterfaces
 {
     public interface ICartRepository
     {
-        Task<ShoppingCart> UpdateCartAsync(int userId, bool isActive);
-        Task<ShoppingCart> CreateCartAsync(int userId, int headerId);
-        Task<ShoppingCart> GetCartAsync(int userId);
-        Task<List<ShoppingCart>> GetCartsAsync();
-        Task<bool> DeleteCartAsync(int userId);
+        Task<Cart> UpdateCartAsync(Cart cart);
+        Task<Cart> CreateCartAsync(int? userId, CartStatus status);
+        Task<Cart> CreateCartAsync(string? sessionId, CartStatus status);
+        Task<Cart> GetCartByUserIdAsync(int? userId, CartStatus? status = CartStatus.Active);
+        Task<Cart> GetCartBySessionIdAsync(string sessionId, CartStatus? status = CartStatus.Active);
+        Task<List<Cart>> GetCartsAsync();
+        Task<bool> DeleteCartAsync(int? userId, string? sessionId);
 
+        Task<Cart> UpdateItemQuantityAsync(int? userId, string? sessionId, int productId, int quantity);
+        Task<CartItem> CreateItemAsync(int cartId, int? userId, string? sessionId, int productId, int quantity, decimal unitPrice);
+
+        Task<bool> RemoveItemAsync(int? userId, string? sessionId, int productId);
+        Task<Cart> UpdateCartStatusAsync(int? userId, string? sessionId, CartStatus status);
+        Task<Cart> GetCartByIdAsync(int cartId, CartStatus status);
     }
 }
