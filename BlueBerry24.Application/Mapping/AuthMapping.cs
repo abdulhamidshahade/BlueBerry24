eec3c7efdd0dc5149a5569b45640319f9ec59b3f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 13:10:53 +0300

    Update AuthMapping with public access and new mappings

--- BlueBerry24.Application/Mapping/AuthMapping.cs
+++ BlueBerry24.Application/Mapping/AuthMapping.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.AuthDtos;
+using BlueBerry24.Domain.Entities.AuthEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +9,21 @@ using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Mapping
 {
-    class AuthMapping : Profile
+    public class AuthMapping : Profile
     {
         public AuthMapping()
         {
+            CreateMap<ApplicationUser, ApplicationUserDto>()
+                .ForMember(desc => desc.Roles, opt => opt.MapFrom(src => src.roles));
 
+            CreateMap<ApplicationUserDto, ApplicationUser>()
+                .ForMember(dest => dest.roles, opt => opt.Ignore());
+
+            CreateMap<ApplicationUser, ApplicationUserWithRolesDto>()
+                .ForMember(dest => dest.Roles, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<ApplicationRoleDto, ApplicationRole>().ReverseMap();
         }
     }
 }
