b0746b4120ed3933a1dd65f9980e75bc9c22f951
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-03 13:12:07 +0300

    Update ShoppingCartMapping with public access and mappings

--- BlueBerry24.Application/Mapping/ShoppingCartMapping.cs
+++ BlueBerry24.Application/Mapping/ShoppingCartMapping.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.ShoppingCartDtos;
+using BlueBerry24.Domain.Entities.ShoppingCartEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Mapping
 {
-    class ShoppingCartMapping : Profile
+    public class ShoppingCartMapping : Profile
     {
+        public ShoppingCartMapping()
+        {
+
+            CreateMap<CartItem, CartItemDto>().ReverseMap();
+            CreateMap<CartCoupon, CartCouponDto>().ReverseMap();
+
+
+            CreateMap<Cart, CartDto>()
+            .ForMember(dest => dest.SubTotal, opt => opt.MapFrom(src => src.SubTotal))
+            .ForMember(dest => dest.DiscountTotal, opt => opt.MapFrom(src => src.DiscountTotal))
+            .ForMember(dest => dest.TaxAmount, opt => opt.MapFrom(src => src.TaxAmount))
+            .ForMember(dest => dest.Total, opt => opt.MapFrom(src => src.Total))
+            .ForMember(dest => dest.CartItems, opt => opt.MapFrom(src => src.CartItems))
+            .ForMember(dest => dest.CartCoupons, opt => opt.MapFrom(src => src.CartCoupons));
+
+            CreateMap<CartDto, Cart>()
+                .ForMember(dest => dest.CartItems, opt => opt.MapFrom(src => src.CartItems))
+                .ForMember(dest => dest.CartCoupons, opt => opt.MapFrom(src => src.CartCoupons))
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Version, opt => opt.Ignore())
+                .ForMember(dest => dest.Order, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore());
+        }
     }
 }
