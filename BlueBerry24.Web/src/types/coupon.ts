4860017aa29040ca6d39b709d1df1b30d9ebbc71
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-04 22:31:09 +0300

    Add coupon-related interfaces and CouponType enum

---
+++ BlueBerry24.Web/src/types/coupon.ts
@@ -0,0 +1,52 @@
+export interface CouponDto {
+  id: number;
+  code: string;
+  discountAmount: number;
+  minimumAmount: number;
+  description: string;
+  isActive: boolean;
+  type: CouponType;
+  value: number;
+  isForNewUsersOnly: boolean;
+}
+
+export interface CreateCouponDto {
+  code: string;
+  discountAmount: number;
+  minimumAmount: number;
+  description: string;
+  isActive: boolean;
+  type: CouponType;
+  value: number;
+  isForNewUsersOnly: boolean;
+}
+
+export interface UpdateCouponDto {
+  id: number;
+  code: string;
+  discountAmount: number;
+  minimumAmount: number;
+  description: string;
+  isActive: boolean;
+  type: CouponType;
+  value: number;
+  isForNewUsersOnly: boolean;
+}
+
+export interface UserCouponDto {
+  userId: number;
+  couponId: number;
+  isUsed: boolean;
+}
+
+export interface AddCouponToUserDto{
+  userId: number;
+  couponId: number;
+}
+
+export enum CouponType {
+  Percentage = 0,
+  FixedAmount = 1,
+  FreeShipping = 2,
+  BuyXGetY = 3
+}
