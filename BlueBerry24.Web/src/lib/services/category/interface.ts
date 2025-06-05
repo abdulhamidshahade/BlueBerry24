9d1e741d13f875bf94894d0f4f6ad71d7bf8edcd
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-05 22:58:30 +0300

    Refactor ICategoryService interface and Add existsById method

--- BlueBerry24.Web/src/lib/services/category/interface.ts
+++ BlueBerry24.Web/src/lib/services/category/interface.ts
@@ -1,11 +1,12 @@
 import { CategoryDto, CreateCategoryDto, UpdateCategoryDto } from "@/types/category";
-export interface ICategoryService{
-    getById(id: number): Promise<CategoryDto>;
-    getByName(name: string): Promise<CategoryDto>;
-    getAll(): Promise<CategoryDto[]>;
-    create(data: CreateCategoryDto): Promise<CategoryDto>;
-    update(id: number, data: UpdateCategoryDto): Promise<CategoryDto>;
-    delete(id: number): Promise<boolean>;
-    exists(id: number): Promise<boolean>;
-    existsByName(name: string): Promise<boolean>;
-}
+
+export interface ICategoryService {
+  getAll(): Promise<CategoryDto[]>;
+  getById(id: number): Promise<CategoryDto>;
+  getByName(name: string): Promise<CategoryDto>;
+  create(data: CreateCategoryDto): Promise<CategoryDto>;
+  update(id: number, data: UpdateCategoryDto): Promise<CategoryDto>;
+  delete(id: number): Promise<boolean>;
+  existsById(id: number): Promise<boolean>;
+  existsByName(name: string): Promise<boolean>;
+} 
