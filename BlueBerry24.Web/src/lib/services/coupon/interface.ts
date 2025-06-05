256f0aa82b2d9620670a4f44c7a10ac37ac8ae0b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-06 02:50:09 +0300

    Add ICouponService interface for coupon management

---
+++ BlueBerry24.Web/src/lib/services/coupon/interface.ts
@@ -0,0 +1,21 @@
+import { User } from "@/types/auth";
+import { CouponDto, CreateCouponDto, UpdateCouponDto, UserCouponDto } from "@/types/coupon";
+
+export interface ICouponService {
+  getAll(): Promise<CouponDto[]>;
+  getById(id: number): Promise<CouponDto>;
+  getByCode(code: string): Promise<CouponDto>;
+  getUserCoupons(userId: number): Promise<CouponDto[]>;
+  getUsersByCouponId(couponId: number): Promise<User[]>;
+  create(data: CreateCouponDto): Promise<CouponDto>;
+  update(id: number, data: UpdateCouponDto): Promise<CouponDto>;
+  delete(id: number): Promise<boolean>;
+  existsById(id: number): Promise<boolean>;
+  existsByCode(code: string): Promise<boolean>;
+  addCouponToUser(userId: number, couponId: number): Promise<UserCouponDto>;
+  disableUserCoupon(userId: number, couponId: number): Promise<boolean>;
+  hasUserUsedCoupon(userId: number, couponCode: string): Promise<boolean>;
+  addCouponToSpecificUsers(couponId: number, userIds: number[]): Promise<boolean>;
+  addCouponToAllUsers(couponId: number): Promise<boolean>;
+  addCouponToNewUsers(couponId: number): Promise<boolean>;
+}
