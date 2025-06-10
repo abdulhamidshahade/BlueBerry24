c4ac6d46de92d1bf910ded63265201eafb79558e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-10 23:32:16 +0300

    Add PaymentStatus enum constant

---
+++ BlueBerry24.Domain/Constants/PaymentStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Constants
+{
+    public enum PaymentStatus
+    {
+        Pending = 0,
+        Processing = 1,
+        Completed = 2,
+        Failed = 3,
+        Cancelled = 4,
+        Refunded = 5,
+        PartiallyRefunded = 6,
+        Disputed = 7,
+        Expired = 8
+    }
+}
