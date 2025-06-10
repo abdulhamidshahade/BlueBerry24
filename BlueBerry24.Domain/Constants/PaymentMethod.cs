36cb79bf80e04c321aa12bc86b6ac7057ad7be87
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-10 23:32:01 +0300

    Add PaymentMethod enum constant

---
+++ BlueBerry24.Domain/Constants/PaymentMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Domain.Constants
+{
+    public enum PaymentMethod
+    {
+        CreditCard = 0,
+        DebitCard = 1,
+        PayPal = 2,
+        BankTransfer = 3,
+        DigitalWallet = 4,
+        Cryptocurrency = 5,
+        GiftCard = 6,
+        StoreCredit = 7,
+        CashOnDelivery = 8
+    }
+}
