567ff32337241100a881a9f3dad605b3cca08808
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-14 18:53:53 +0300

    Add IInventoryService interface for inventory management

---
+++ BlueBerry24.Web/src/lib/services/inventory/interface.ts
@@ -0,0 +1,15 @@
+import { ProductDto } from "@/types/product";
+import { AddStockRequest, AdjustStockRequest } from "./service";
+import { InventoryLogDto } from "@/types/inventory";
+
+export interface IInventoryService{
+
+    addStock(request: AddStockRequest) : Promise<boolean>;
+    adjustStock(request: AdjustStockRequest) : Promise<boolean>;
+    getProductWithStockInfo(productId: number) : Promise<ProductDto>;
+    getLowStockProducts(limit: number) : Promise<ProductDto[]>;
+    getInventoryHistory(productId: number, limit: number) : Promise<InventoryLogDto[]>;
+    isInStock(productId: number, quality: number) : Promise<boolean>;
+    reserveStock(productId: number, quantity: number, referenceId: number, referenceType: string) : Promise<string>;
+    releaseReservedStock(productId: number, quantity: number, referenceId: number, referenceType: string) : Promise<boolean>;
+}
