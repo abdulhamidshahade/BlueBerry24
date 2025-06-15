5e3ce871462fec02aa82dbff4285babb5124eb88
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 22:35:47 +0300

    Add SendEmailRequest class

---
+++ BlueBerry24.Application/Dtos/EmailDtos/SendEmailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry24.Application.Dtos.EmailDtos
+{
+    public class SendEmailRequest
+    {
+        public string Recipient { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public bool IsBodyHtml { get; set; } = true;
+    }
+}
