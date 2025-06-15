0a45db8e6247f44f611d35addfb11fc9e87b8ff8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 22:36:11 +0300

    Add IMailService interface

---
+++ BlueBerry24.Application/Services/Interfaces/EmailServiceInterfaces/IMailService.cs
@@ -0,0 +1,11 @@
+﻿using BlueBerry24.Application.Dtos.EmailDtos;
+
+namespace BlueBerry24.Application.Services.Interfaces.EmailServiceInterfaces
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(SendEmailRequest request);
+        Task SendPasswordResetEmailAsync(string email, string resetToken, string resetUrl);
+        Task SendEmailConfirmationAsync(string email, string confirmationToken, string confirmationUrl, string userName);
+    }
+}
