409500325b99fe055131c641e0b96cae54c9fccc
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 23:18:48 +0300

    Add AppSettings class for application configuration
    Add a new `AppSettings` class in the
    `BlueBerry24.Application.Config` namespace. This class
    includes three string properties: `BaseUrl`, `AppName`,
    and `Version`, with public getters and setters for
    configuration management.

---
+++ BlueBerry24.Application/Config/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Application.Config
+{
+    public class AppSettings
+    {
+        public string BaseUrl { get; set; }
+        public string AppName { get; set; }
+        public string Version { get; set; }
+    }
+}
