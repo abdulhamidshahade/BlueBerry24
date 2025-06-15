5fbc0e741120d26a62d1f1d26c0c383e45788aca
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 22:38:06 +0300

    Add new methods to IAuthService for account recovery
    Update the IAuthService interface to
    include four new asynchronous methods: ForgotPasswordAsync,
    ResetPasswordAsync, ConfirmEmailAsync, and
    ResendConfirmationEmailAsync. These methods enhance user
    authentication and account recovery functionalities.

--- BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
@@ -6,5 +6,9 @@ namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
     {
         Task<RegisterResponseDto> Register(RegisterRequestDto requestDto);
         Task<LoginResponseDto> Login(LoginRequestDto requestDto);
+        Task<bool> ForgotPasswordAsync(ForgotPasswordRequestDto requestDto);
+        Task<bool> ResetPasswordAsync(ResetPasswordDto requestDto);
+        Task<bool> ConfirmEmailAsync(EmailConfirmationDto confirmationDto);
+        Task<bool> ResendConfirmationEmailAsync(string email);
     }
 }
