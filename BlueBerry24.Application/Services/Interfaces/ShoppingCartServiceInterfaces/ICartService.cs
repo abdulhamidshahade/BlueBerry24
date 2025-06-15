9338353346e5f977507efd9b38dd98984d934f96
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-16 02:13:13 +0300

    Refactor ICartService methods for improved signatures
    - Change `GetCartByUserIdAsync` to require a non-nullable `int` for `userId`.
    - Add `GetCartByIdAsync` with non-nullable parameters.
    - Update `GetItemAsync` to accept a non-nullable `int cartId`.
    - Modifie `CompleteCartAsync` to remove the `sessionId` parameter.
    - Remove the old `GetCartByIdAsync` method with a different signature.
    - Add `MergeCartAsync` with parameters for `userId` and `sessionId`.

--- BlueBerry24.Application/Services/Interfaces/ShoppingCartServiceInterfaces/ICartService.cs
+++ BlueBerry24.Application/Services/Interfaces/ShoppingCartServiceInterfaces/ICartService.cs
@@ -10,22 +10,22 @@ namespace BlueBerry24.Application.Services.Interfaces.ShoppingCartServiceInterfa
 {
     public interface ICartService
     {
-        Task<CartDto> GetCartByUserIdAsync(int? userId, CartStatus? status = CartStatus.Active);
+        Task<CartDto> GetCartByUserIdAsync(int userId, CartStatus? status = CartStatus.Active);
         Task<CartDto> GetCartBySessionIdAsync(string sessionId, CartStatus? status = CartStatus.Active);
-        Task<CartItemDto> GetItemAsync(string cartId, int productId);
+        Task<CartDto> GetCartByIdAsync(int cartId, CartStatus status);
+        Task<CartItemDto> GetItemAsync(int cartId, int productId);
         Task<CartDto> CreateCartAsync(int? userId, string? sessionId);
         Task<CartDto?> AddItemAsync(int cartId, int? userId, string? sessionId, int productId, int quantity);
         Task<CartDto> UpdateItemQuantityAsync(int cartId, int? userId, string? sessionId, int productId, int quantity);
         Task<bool> RemoveItemAsync(int cartId, int? userId, string? sessionId, int productId);
         Task<bool> ClearCartAsync(int cartId, int? userId, string? sessionId);
-        Task<bool> CompleteCartAsync(int cartId, int? userId, string? sessionId);
+        Task<bool> CompleteCartAsync(int cartId, int? userId);
         Task<bool> ConvertCartAsync(int cartId);
         Task<bool> HandleAbandonedCartAsync(int cartId);
         Task<int> CleanupExpiredCartsAsync();
         Task<CartDto> RefreshCartAsync(int cartId);
         Task<CartDto> ApplyCouponAsync(int cartId, int? userId, string couponCode);
         Task<CartDto> RemoveCouponAsync(int cartId, int? userId, string? sessionId, int couponId);
-        Task<CartDto> GetCartByIdAsync(int cartId, CartStatus status);
-
+        Task<CartDto> MergeCartAsync(int userId, string sessionId);
     }
 }
