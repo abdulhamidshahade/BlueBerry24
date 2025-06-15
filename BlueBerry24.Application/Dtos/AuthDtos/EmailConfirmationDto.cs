c8ccfa6df077b7c75f7af9e0e8029e035a2bfb57
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 22:33:55 +0300

    Add EmailConfirmationDto class

---
+++ BlueBerry24.Application/Dtos/AuthDtos/EmailConfirmationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class EmailConfirmationDto
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+    }
+}
