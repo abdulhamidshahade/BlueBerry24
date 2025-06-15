d9383159529e60a93a0cff39ada98c613b10e974
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-15 22:35:04 +0300

    Add ResetPasswordDto class

---
+++ BlueBerry24.Application/Dtos/AuthDtos/ResetPasswordDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string NewPassword { get; set; }
+
+
+        [Required]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
