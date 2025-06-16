9411294b4c2c4f5007771870295daf810f05304f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-17 00:37:24 +0300

    Add IUserService interface to define user-related operations

---
+++ BlueBerry24.Web/src/lib/services/user/interface.ts
@@ -0,0 +1,16 @@
+import { User } from "@/types/user";
+import { CreateUserData, UpdateUserData } from "@/types/user";
+
+export interface IUserService {
+  getAll(): Promise<User[]>;
+  getById(id: number): Promise<User>;
+  
+  lockAccount(userId: number, lockoutEnd?: string): Promise<boolean>;
+  unlockAccount(userId: number): Promise<boolean>;
+  resetPassword(userId: number, newPassword: string): Promise<boolean>;
+  verifyEmail(userId: number): Promise<boolean>;
+  updateUser(userId: number, userData: UpdateUserData): Promise<boolean>;
+  createUser(userData: CreateUserData): Promise<User>;
+  deleteUser(userId: number): Promise<boolean>;
+}
+
