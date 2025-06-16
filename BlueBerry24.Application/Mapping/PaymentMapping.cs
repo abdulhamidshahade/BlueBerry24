62aeb1b92c92abd3496eac1bf2565edc9970dd6b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-16 09:34:20 +0300

    Add PaymentMapping class for mapping between Payment and PaymentDto

---
+++ BlueBerry24.Application/Mapping/PaymentMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlueBerry24.Application.Dtos.PaymentDtos;
+using BlueBerry24.Domain.Entities.PaymentEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Mapping
+{
+    public class PaymentMapping : Profile
+    {
+        public PaymentMapping()
+        {
+            CreateMap<Payment, PaymentDto>().ReverseMap();
+        }
+    }
+}
