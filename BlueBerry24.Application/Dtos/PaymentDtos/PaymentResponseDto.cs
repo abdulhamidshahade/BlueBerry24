511e562c08aef0fffdd1ba1e1c2580c67052a067
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-16 09:33:09 +0300

    Add PaymentResponseDto class for payment response data transfer

---
+++ BlueBerry24.Application/Dtos/PaymentDtos/PaymentResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using BlueBerry24.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.PaymentDtos
+{
+    public class PaymentResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public PaymentDto? Payment { get; set; }
+        public string? TransactionId { get; set; }
+        public PaymentStatus Status { get; set; }
+        public string? RedirectUrl { get; set; }
+        public Dictionary<string, object>? Metadata { get; set; }
+    }
+}
