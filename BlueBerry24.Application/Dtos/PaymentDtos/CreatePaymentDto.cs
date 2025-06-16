c7e98946a32e4613e788738b76bdbb6859bd4bf8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-06-16 09:32:28 +0300

    Add CreatePaymentDto class for payment creation data transfer
    Add CreatePaymentDto class, which encapsulates the necessary properties for creating a payment, including order details, payment method, payer information, and billing address. The class includes validation attributes for email and initializes default values for certain properties.

---
+++ BlueBerry24.Application/Dtos/PaymentDtos/CreatePaymentDto.cs
@@ -0,0 +1,55 @@
+﻿using BlueBerry24.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.PaymentDtos
+{
+    public class CreatePaymentDto
+    {
+        public int? OrderId { get; set; }
+
+
+        public PaymentMethod Method { get; set; }
+
+      
+        public string Provider { get; set; } = string.Empty;
+
+       
+        
+        public decimal Amount { get; set; }
+
+        public string Currency { get; set; } = "USD";
+
+        public string? ProviderPaymentMethodId { get; set; }
+
+        // Payment Details
+        public string? CardLast4 { get; set; }
+        public string? CardBrand { get; set; }
+
+        [EmailAddress]
+        public string? PayerEmail { get; set; } = string.Empty;
+
+        public string? PayerName { get; set; } = string.Empty;
+  
+        public string? BillingAddress1 { get; set; } = string.Empty;
+        public string? BillingAddress2 { get; set; }
+
+        public string BillingCity { get; set; } = string.Empty;
+
+       
+        public string BillingState { get; set; } = string.Empty;
+
+    
+        public string BillingPostalCode { get; set; } = string.Empty;
+
+  
+        public string BillingCountry { get; set; } = string.Empty;
+
+        public string? Metadata { get; set; }
+        public string? Notes { get; set; }
+    }
+}
