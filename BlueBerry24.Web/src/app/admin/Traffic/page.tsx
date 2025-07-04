b1360a22212c054ecda455bd8fd4cb8866f79ab5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-07-04 19:24:02 +0300

    Add Traffic Page with Under Development Notification
    Create Traffic page that currently displays an under development message. This page includes metadata for SEO purposes, indicating that updates will be available soon. This addition continues the development of the BlueBerry24 platform's administrative features.

---
+++ BlueBerry24.Web/src/app/admin/Traffic/page.tsx
@@ -0,0 +1,13 @@
+import UnderDevelopment from "@/components/shared/UnderDevelopment";
+
+
+export default function TrafficPage() {
+  return (
+    <UnderDevelopment />
+  );
+}
+
+export const metadata = {
+  title: 'Traffic | BlueBerry24',
+  description: 'This page is currently under development. Please check back soon for updates.',
+};
