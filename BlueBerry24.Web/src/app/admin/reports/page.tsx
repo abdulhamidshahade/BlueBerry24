8c9dcd5af691fbf0830ef32b000f8fa52f07e1cd
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-07-04 19:23:45 +0300

    Add Reports Page with Under Development Notification
    Create  Reports page that currently displays an under development message. This page includes metadata for SEO purposes, indicating that updates will be available soon. This addition sets the stage for future reporting features in the BlueBerry24 platform.

---
+++ BlueBerry24.Web/src/app/admin/reports/page.tsx
@@ -0,0 +1,13 @@
+import UnderDevelopment from "@/components/shared/UnderDevelopment";
+
+export default function ReportsPage(){
+  return(
+    <UnderDevelopment />
+  )
+}
+
+
+export const metadata = {
+  title: 'Reports | BlueBerry24',
+  description: 'This page is currently under development. Please check back soon for updates.',
+};
