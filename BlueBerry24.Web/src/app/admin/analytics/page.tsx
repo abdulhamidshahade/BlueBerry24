29aad376507ba961143f998e8871d182a455bd74
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-07-04 19:23:17 +0300

    Add Analytics Page with Under Development Notification
    Create Analytics page that currently displays an under development message. This page includes metadata for SEO purposes, indicating that updates will be available soon. This addition lays the groundwork for future analytics features in the BlueBerry24 platform.

---
+++ BlueBerry24.Web/src/app/admin/analytics/page.tsx
@@ -0,0 +1,12 @@
+import UnderDevelopment from '@/components/shared/UnderDevelopment';
+
+export default function AnalyticsPage() {
+  return(
+    <UnderDevelopment />
+  )
+}
+
+export const metadata = {
+  title: 'Analytics | BlueBerry24',
+  description: 'This page is currently under development. Please check back soon for updates.',
+};
