31c8ffa41f8b7bd2de0d1b1ab316192410bad314
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-08-03 04:07:04 +0300

    Add RequireHttps property to JwtOptions for enhanced security configuration

--- BlueBerry24.Domain/Entities/AuthEntities/JwtOptions.cs
+++ BlueBerry24.Domain/Entities/AuthEntities/JwtOptions.cs
@@ -5,5 +5,6 @@
         public string SecretKey { get; set; } = string.Empty;
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
+        public bool RequireHttps { get; set; }
     }
 }
