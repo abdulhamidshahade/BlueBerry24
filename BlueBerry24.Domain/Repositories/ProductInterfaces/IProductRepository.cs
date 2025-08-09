5174e7f7e1b0446f442154067f5daeef9b3e99c8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-08-09 23:54:22 +0300

    Update IProductRepository with new filtering methods
    Modifie the `GetAllAsync` method to return `IReadOnlyList<Product>`.
    Added `GetTotalCountAsync` and `GetFilteredCountAsync` for counting products.
    Introduce `GetFilteredAsync` to retrieve products with various filter options.

--- BlueBerry24.Domain/Repositories/ProductInterfaces/IProductRepository.cs
+++ BlueBerry24.Domain/Repositories/ProductInterfaces/IProductRepository.cs
@@ -4,14 +4,21 @@ namespace BlueBerry24.Domain.Repositories.ProductInterfaces
 {
     public interface IProductRepository
     {
+        Task<IReadOnlyList<Product>> GetAllAsync();
         Task<Product> GetByIdAsync(int id);
         Task<Product> GetByNameAsync(string name);
-        Task<IEnumerable<Product>> GetAllAsync();
         Task<Product> CreateAsync(Product product);
         Task<Product> UpdateAsync(int id, Product product);
         Task<bool> DeleteAsync(Product product);
         Task<bool> ExistsByIdAsync(int id);
         Task<bool> ExistsByNameAsync(string name);
+        Task<int> GetTotalCountAsync();
 
+        Task<IReadOnlyList<Product>> GetFilteredAsync(string? searchTerm = null, string? category = null,
+            string? sortBy = "name", decimal? minPrice = null, decimal? maxPrice = null,
+            bool? isActive = true, int pageNumber = 1, int pageSize = 10);
+
+        Task<int> GetFilteredCountAsync(string? searchTerm = null, string? category = null,
+            decimal? minPrice = null, decimal? maxPrice = null, bool? isActive = true);
     }
 }
