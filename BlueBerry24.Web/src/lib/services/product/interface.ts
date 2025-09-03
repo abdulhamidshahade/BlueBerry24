ed375bf7e75054ae68444fcf4e418382a0eb3e8b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-09-03 23:37:19 +0300

    Update IProductService interface with pagination and filtering methods
    - Add getPaginated method to support product retrieval with filtering options.
    - Reorder getAll method to maintain interface consistency.

--- BlueBerry24.Web/src/lib/services/product/interface.ts
+++ BlueBerry24.Web/src/lib/services/product/interface.ts
@@ -1,12 +1,14 @@
 import { CreateProductDto, ProductDto, UpdateProductDto } from "../../../types/product";
+import { PaginationDto, ProductFilterDto } from "../../../types/pagination";
 
 export interface IProductService{
     getById(id: number): Promise<ProductDto>;
     getByName(name: string): Promise<ProductDto>;
-    getAll(): Promise<ProductDto[]>;
+    getPaginated(filter: ProductFilterDto): Promise<PaginationDto<ProductDto>>;
     create(data: CreateProductDto, categories: number[]): Promise<ProductDto>;
     update(id: number, data: UpdateProductDto, categories: number[]): Promise<ProductDto>;
     delete(id: number): Promise<boolean>;
     existsById(id: number): Promise<boolean>;
     existsByName(name: string): Promise<boolean>;
+    getAll(): Promise<ProductDto[]>;
 }
