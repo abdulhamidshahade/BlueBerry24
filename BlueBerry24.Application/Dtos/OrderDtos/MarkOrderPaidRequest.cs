efe38c248a40d74df035a39001c7fa7ecd6768b4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-10-06 23:38:56 +0300

    Add MarkOrderPaidRequest DTO for handling payment confirmation
    - Introduce MarkOrderPaidRequest class to encapsulate details for marking an order as paid.
    - Include properties for PaymentTransactionId and PaymentProvider to enhance order processing capabilities.
    - Structure the DTO for clarity and maintainability in payment operations.

---
+++ BlueBerry24.Application/Dtos/OrderDtos/MarkOrderPaidRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.Application.Dtos.OrderDtos
+{
+    public class MarkOrderPaidRequest
+    {
+        public int PaymentTransactionId { get; set; }
+        public string PaymentProvider { get; set; } = string.Empty;
+    }
+
+}
