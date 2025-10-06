4fb74800feec230fd6b7121a351618cc53f5e8f1
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-10-06 23:39:07 +0300

    Add UpdateOrderStatusRequest DTO for updating order status
    - Introduce UpdateOrderStatusRequest class to encapsulate details for updating an order's status.
    - Include a property for NewStatus to enhance order management capabilities.
    - Structure the DTO for clarity and maintainability in order status updates.

---
+++ BlueBerry24.Application/Dtos/OrderDtos/UpdateOrderStatusRequest.cs
@@ -0,0 +1,10 @@
+﻿using BlueBerry24.Domain.Constants;
+
+namespace BlueBerry24.Application.Dtos.OrderDtos
+{
+    public class UpdateOrderStatusRequest
+    {
+        public OrderStatus NewStatus { get; set; }
+    }
+
+}
