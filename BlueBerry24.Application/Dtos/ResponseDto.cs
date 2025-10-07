737b8d8ccfa1b2c8c4fa725d92cc0b53e0c615e8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-10-07 23:53:47 +0300

    Update ResponseDto to allow nullable StatusMessage property
    - Change StatusMessage property to nullable string to accommodate scenarios where a status message may not be provided.
    - Enhance the flexibility of the ResponseDto for various response scenarios.

--- BlueBerry24.Application/Dtos/ResponseDto.cs
+++ BlueBerry24.Application/Dtos/ResponseDto.cs
@@ -10,7 +10,7 @@ namespace BlueBerry24.Application.Dtos
     {
         public bool IsSuccess { get; set; }
         public int StatusCode { get; set; }
-        public string StatusMessage { get; set; }
+        public string? StatusMessage { get; set; }
         public T? Data { get; set; }
         public List<string> Errors { get; set; } = new();
     }
