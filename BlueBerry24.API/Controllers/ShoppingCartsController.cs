17832d2049bb2350db9638101967c07e17808bf3
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-10-10 23:51:51 +0300

    Move ApplyCouponRequest DTO to a dedicated namespace for improved organization
    - Remove the ApplyCouponRequest class from ShoppingCartsController to enhance code structure.
    - Add ApplyCouponRequest class in the BlueBerry24.Application.Dtos.ShoppingCartDtos namespace for better separation of concerns and maintainability.

--- BlueBerry24.API/Controllers/ShoppingCartsController.cs
+++ BlueBerry24.API/Controllers/ShoppingCartsController.cs
@@ -704,10 +704,4 @@ namespace BlueBerry24.API.Controllers
 
     }
 
-    public class ApplyCouponRequest
-    {
-        public string CouponCode { get; set; } = string.Empty;
-    }
-
 }
-
---
+++ BlueBerry24.Application/Dtos/ShoppingCartDtos/ApplyCouponRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueBerry24.API.Controllers
+{
+    public class ApplyCouponRequest
+    {
+        public string CouponCode { get; set; } = string.Empty;
+    }
+
+}
+
