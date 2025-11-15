5026d7f40c041a7056fee03605a1a232d11a687a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-11-15 22:47:46 +0300

    Remove unused using directives in OrderTotal to enhance code cleanliness and maintainability.

--- BlueBerry24.Domain/Entities/OrderEntities/OrderTotal.cs
+++ BlueBerry24.Domain/Entities/OrderEntities/OrderTotal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlueBerry24.Domain.Entities.OrderEntities
+﻿namespace BlueBerry24.Domain.Entities.OrderEntities
 {
     public class OrderTotal
     {
