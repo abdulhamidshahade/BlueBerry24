09591ac0d17a4a1b11cc2f12a57bd62a8d328429
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-11-19 10:10:58 +0300

    Remove unused using directives in CategoryDto to enhance code cleanliness and maintainability.

--- BlueBerry24.Application/Dtos/CategoryDtos/CategoryDto.cs
+++ BlueBerry24.Application/Dtos/CategoryDtos/CategoryDto.cs
@@ -1,9 +1,4 @@
 ﻿using BlueBerry24.Application.Dtos.ProductDtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Dtos.CategoryDtos
 {
