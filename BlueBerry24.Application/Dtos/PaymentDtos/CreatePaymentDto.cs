bfd6ea63c1773608fe03e7319a9f2e8d1bb1d90b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-11-20 23:58:14 +0300

    Remove unused using directives in CreatePaymentDto to enhance code cleanliness and maintainability.

--- BlueBerry24.Application/Dtos/PaymentDtos/CreatePaymentDto.cs
+++ BlueBerry24.Application/Dtos/PaymentDtos/CreatePaymentDto.cs
@@ -1,10 +1,5 @@
 ﻿using BlueBerry24.Domain.Constants;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Dtos.PaymentDtos
 {
