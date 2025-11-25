55687a06e0f8539d6346dd20907415fc4a958b0d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-11-25 23:25:47 +0300

    Remove unused using directives in BulkAssignRoleRequest to enhance code cleanliness and maintainability.

--- BlueBerry24.Application/Dtos/AuthDtos/BulkAssignRoleRequest.cs
+++ BlueBerry24.Application/Dtos/AuthDtos/BulkAssignRoleRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlueBerry24.Application.Dtos.AuthDtos
+﻿namespace BlueBerry24.Application.Dtos.AuthDtos
 {
     public class BulkAssignRoleRequest
     {
