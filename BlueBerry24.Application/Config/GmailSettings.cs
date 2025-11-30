fe7d5925be0208305d3f01420b0f534a02f5491a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-11-30 17:41:08 +0300

    Rename GmailOption to GmailSettings

--- BlueBerry24.Application/Config/GmailSettings.cs
+++ BlueBerry24.Application/Config/GmailSettings.cs
@@ -1,7 +1,8 @@
 ﻿namespace BlueBerry24.Application.Config
 {
-    public class GmailOption
+    public class GmailSettings
     {
+        public const string Name = "GmailSettings";
         public string Email { get; set; }
         public string Password { get; set; }
         public string Host { get; set; }
