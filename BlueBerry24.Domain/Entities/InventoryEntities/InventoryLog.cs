ecdabb08c7c7282165588ef1bca5ff12f28adf1a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-12-11 23:45:30 +0300

    Remove unused usings from InventoryLog.cs
    Cleaned up InventoryLog.cs by removing unnecessary using directives (System, System.Collections.Generic, System.Linq, System.Text, System.Threading.Tasks) to improve code clarity and maintainability.

--- BlueBerry24.Domain/Entities/InventoryEntities/InventoryLog.cs
+++ BlueBerry24.Domain/Entities/InventoryEntities/InventoryLog.cs
@@ -1,11 +1,6 @@
 ﻿using BlueBerry24.Domain.Constants;
 using BlueBerry24.Domain.Entities.Base;
 using BlueBerry24.Domain.Entities.ProductEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Domain.Entities.InventoryEntities
 {
