b3658f69c246ede059ae4470b821eb5291c51d97
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-12-30 00:33:27 +0300

    Update PaymentsController route to 'api/payments' for consistent API structure

--- BlueBerry24.API/Controllers/PaymentsController.cs
+++ BlueBerry24.API/Controllers/PaymentsController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlueBerry24.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/payments")]
     [ApiController]
     public class PaymentsController : BaseController
     {
