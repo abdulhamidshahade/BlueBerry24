eca8a07e4671fa2ba8e66957948afa8baef10f19
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-07 22:25:35 +0300

    Remove unused namespaces from AssemblyMarker for cleaner code

--- BlueBerry24.Application/Mapping/AssemblyMarker.cs
+++ BlueBerry24.Application/Mapping/AssemblyMarker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlueBerry24.Application.Mapping
+﻿namespace BlueBerry24.Application.Mapping
 {
     public class AssemblyMarker
     {
