c9d7615eb871545de2c4c2a08f5308a7e8181862
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-07 22:25:48 +0300

    Remove unused namespaces from PaymentMapping for cleaner code

--- BlueBerry24.Application/Mapping/PaymentMapping.cs
+++ BlueBerry24.Application/Mapping/PaymentMapping.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using BlueBerry24.Application.Dtos.PaymentDtos;
 using BlueBerry24.Domain.Entities.PaymentEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Mapping
 {
