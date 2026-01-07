be7c2964fca3098e82b9b129977a4c9782bbe104
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-07 22:25:06 +0300

    Remove unused namespaces from OrderMapping for cleaner code

--- BlueBerry24.Application/Mapping/OrderMapping.cs
+++ BlueBerry24.Application/Mapping/OrderMapping.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using BlueBerry24.Application.Dtos.OrderDtos;
 using BlueBerry24.Domain.Entities.OrderEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Mapping
 {
