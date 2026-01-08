4e4ab37ecce9e409ef48ad165f807020247d3237
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-08 22:34:39 +0300

    Remove unused namespaces from ProductMapping for cleaner code

--- BlueBerry24.Application/Mapping/ProductMapping.cs
+++ BlueBerry24.Application/Mapping/ProductMapping.cs
@@ -1,12 +1,6 @@
 ﻿using AutoMapper;
 using BlueBerry24.Application.Dtos.ProductDtos;
 using BlueBerry24.Domain.Entities.ProductEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BlueBerry24.Application.Mapping
 {
     public class ProductMapping : Profile
