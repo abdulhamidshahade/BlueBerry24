0c05319fda98a592fdb437671565986ac29f0dec
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-10 23:10:27 +0300

    Remove unused namespaces from ResponseDto for cleaner code

--- BlueBerry24.Application/Dtos/ResponseDto.cs
+++ BlueBerry24.Application/Dtos/ResponseDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlueBerry24.Application.Dtos
+﻿namespace BlueBerry24.Application.Dtos
 {
     public class ResponseDto<T>
     {
