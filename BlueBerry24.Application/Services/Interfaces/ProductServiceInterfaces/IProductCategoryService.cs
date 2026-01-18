c0e54b7753a1f01cc040fe0f0fd69d78cb0815a1
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-18 23:15:42 +0300

    Remove unused using directives from IProductCategoryService.cs
    Cleaned up unnecessary imports to improve code clarity and maintainability.

--- BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductCategoryService.cs
+++ BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductCategoryService.cs
@@ -1,10 +1,4 @@
 ﻿using BlueBerry24.Application.Dtos.ProductDtos;
-using BlueBerry24.Domain.Entities.ProductEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces
 {
