5a96196c779dd2a8cc3873a14be49ef19185f0f8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-18 23:16:02 +0300

    Clean up unused usings in IProductService.cs
    Removed unnecessary System namespace usings to improve code clarity and maintain only relevant application-specific imports.

--- BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductService.cs
+++ BlueBerry24.Application/Services/Interfaces/ProductServiceInterfaces/IProductService.cs
@@ -1,10 +1,5 @@
 ﻿using BlueBerry24.Application.Dtos;
 using BlueBerry24.Application.Dtos.ProductDtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Services.Interfaces.ProductServiceInterfaces
 {
