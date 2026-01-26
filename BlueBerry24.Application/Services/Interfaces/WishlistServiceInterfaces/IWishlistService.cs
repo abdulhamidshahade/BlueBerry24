353afd6475e55c6136f08099dbab973bdd2bee8e
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-01-26 23:46:16 +0300

    Clean up unused usings in IWishlistService.cs
    Removed unnecessary using directives to improve code clarity and maintainability, keeping only the relevant Dtos namespace.

--- BlueBerry24.Application/Services/Interfaces/WishlistServiceInterfaces/IWishlistService.cs
+++ BlueBerry24.Application/Services/Interfaces/WishlistServiceInterfaces/IWishlistService.cs
@@ -1,9 +1,4 @@
 ﻿using BlueBerry24.Application.Dtos.WishlistDtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBerry24.Application.Services.Interfaces.WishlistServiceInterfaces
 {
