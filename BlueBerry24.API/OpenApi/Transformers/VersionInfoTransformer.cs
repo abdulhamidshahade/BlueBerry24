dce41cce84de206409d1f82dc082603a1473a856
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-08 23:51:26 +0300

    Add VersionInfoTransformer to set API version and title in OpenAPI documentation
    Added a new VersionInfoTransformer class that implements IOpenApiDocumentTransformer. This transformer updates the OpenAPI document's version and title based on the document name, enhancing the clarity of API versioning in the documentation.

---
+++ BlueBerry24.API/OpenApi/Transformers/VersionInfoTransformer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace BlueBerry24.API.OpenApi.Transformers;
+
+public sealed class VersionInfoTransformer : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        var version = context.DocumentName;
+
+        document.Info.Version = version;
+        document.Info.Title = $"BlueBerry24 API {version}";
+
+        return Task.CompletedTask;
+    }
+}
