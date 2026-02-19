a66ebc6e395459159fd5ada24e25cac229df3bac
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-19 23:48:55 +0300

    Add PasswordStrength enum to AuthServiceConcretes namespace
    Introduced a PasswordStrength enum with values Weak, Medium, and Strong to represent password strength levels. Included standard using directives at the top of the file. This will help standardize password strength evaluation across the application.

---
+++ BlueBerry24.Application/Services/Concretes/AuthServiceConcretes/PasswordStrength.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Services.Concretes.AuthServiceConcretes
+{
+    public enum PasswordStrength
+    {
+        Weak,
+        Medium,
+        Strong
+    }
+}
