30a6ad4280f551ef50b9c216233d5231384fc891
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-22 02:02:15 +0300

    Add RegisterRequestDtoValidator for validating registration requests
    Implemented a new validator for RegisterRequestDto using FluentValidation. The validator enforces rules for the FirstName field, ensuring it is not null or empty, contains only letters, and adheres to specified length constraints. This addition enhances input validation during user registration.

---
+++ BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/RegisterRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos.AuthValidations
+{
+    public class RegisterRequestDtoValidator : AbstractValidator<RegisterRequestDto>
+    {
+        public RegisterRequestDtoValidator()
+        {
+            RuleFor(fn => fn.FirstName)
+                .NotNull().WithMessage("First name is required.")
+                .NotEmpty().WithMessage("First name is required.")
+                .Matches(@"^[a-zA-Z\s'-]+$").WithMessage("First name must contain only letters.")
+                .MinimumLength(2).WithMessage("First name must be at least 2 characters long.")
+                .MaximumLength(50).WithMessage("First name must be at most 50 characters long.");
+        }
+    }
+}
