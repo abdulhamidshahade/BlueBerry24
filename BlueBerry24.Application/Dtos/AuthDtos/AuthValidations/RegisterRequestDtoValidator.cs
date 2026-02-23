460dcf985deaab730a10d6429ca430a3e180bb71
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-24 02:23:20 +0300

    Update RegisterRequestDtoValidator with additional validation rules
    Expanded the RegisterRequestDtoValidator to include validation for LastName, UserName, Email, and Password fields. Each field now has specific rules for null/empty checks, format validation, and length constraints, improving the overall robustness of user registration input validation.

--- BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/RegisterRequestDtoValidator.cs
+++ BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/RegisterRequestDtoValidator.cs
@@ -12,6 +12,31 @@ namespace BlueBerry24.Application.Dtos.AuthDtos.AuthValidations
                 .Matches(@"^[a-zA-Z\s'-]+$").WithMessage("First name must contain only letters.")
                 .MinimumLength(2).WithMessage("First name must be at least 2 characters long.")
                 .MaximumLength(50).WithMessage("First name must be at most 50 characters long.");
+
+            RuleFor(fn => fn.LastName)
+                .NotNull().WithMessage("Last name is required.")
+                .NotEmpty().WithMessage("Last name is required.")
+                .Matches(@"^[a-zA-Z\s'-]+$").WithMessage("Last name must contain only letters.")
+                .MinimumLength(2).WithMessage("Last name must be at least 2 characters long.")
+                .MaximumLength(50).WithMessage("Last name must be at most 50 characters long.");
+
+            RuleFor(fn => fn.UserName)
+                .NotNull().WithMessage("Last name is required.")
+                .NotEmpty().WithMessage("Last name is required.")
+                .Matches(@"^[a-zA-Z0-9\s._'-]+$").WithMessage("Last name must contain only letters.")
+                .MinimumLength(3).WithMessage("Last name must be at least 3 characters long.")
+                .MaximumLength(20).WithMessage("Last name must be at most 20 characters long.");
+
+            RuleFor(fn => fn.Email)
+                .NotNull().WithMessage("Email is required.")
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email must be a valid email address.");
+
+            RuleFor(fn => fn.Password)
+                .NotNull().WithMessage("Password is required.")
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(2).WithMessage("Password must be at least 2 characters long.")
+                .MaximumLength(50).WithMessage("Password must be at most 50 characters long.");
         }
     }
 }
