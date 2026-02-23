53ac19ce30c1d8f6ade406ef1da3231498b6a2c8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-24 02:23:39 +0300

    Add ForgotPasswordRequestDtoValidator for password recovery validation
    Implemented a new validator for ForgotPasswordRequestDto using FluentValidation. The validator enforces rules for the Email field, ensuring it is not null or empty and follows a valid email format. This addition enhances input validation for password recovery requests.

---
+++ BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/ForgotPasswordRequestDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos.AuthValidations
+{
+    public class ForgotPasswordRequestDtoValidator : AbstractValidator<ForgotPasswordRequestDto>
+    {
+        public ForgotPasswordRequestDtoValidator()
+        {
+            RuleFor(fn => fn.Email)
+                .NotNull().WithMessage("Email is required.")
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email must be a valid email address.");
+        }
+    }
+}
