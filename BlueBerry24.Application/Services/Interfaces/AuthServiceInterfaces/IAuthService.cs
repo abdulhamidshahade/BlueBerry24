4f984414270257ca58f42888e2dd240ec6164cee
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-25 00:49:10 +0300

    Update IAuthService interface to return Result type for Register method and adjust ResetPassword method signature

--- BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
+++ BlueBerry24.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
@@ -1,14 +1,15 @@
-﻿using BlueBerry24.Application.Dtos;
+﻿using BlueBerry24.API.Controllers;
+using BlueBerry24.Application.Dtos;
 using BlueBerry24.Application.Dtos.AuthDtos;
 
 namespace BlueBerry24.Application.Services.Interfaces.AuthServiceInterfaces
 {
     public interface IAuthService
     {
-        Task<RegisterResponseDto> Register(RegisterRequestDto requestDto);
+        Task<Result<RegisterResponseDto>> Register(RegisterRequestDto requestDto);
         Task<LoginResponseDto> Login(LoginRequestDto requestDto);
         Task<bool> ForgotPasswordAsync(ForgotPasswordRequestDto requestDto);
-        Task<bool> ResetPasswordAsync(ResetPasswordDto requestDto);
+        Task<bool> ResetPasswordAsync(ResetPasswordRequestDto requestDto);
         Task<bool> ConfirmEmailAsync(EmailConfirmationDto confirmationDto);
         Task<bool> ResendConfirmationEmailAsync(string email);
     }
