cf609630d5e1d8adb1c606ae680ef35004aad66a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-25 00:47:50 +0300

    Add LoginRequestDtoValidator for login request validation
    Implemented a new validator for LoginRequestDto using FluentValidation. The validator enforces rules for the Email and Password fields, ensuring they are not null or empty and that the email follows a valid format. This addition enhances input validation for login requests.

---
+++ BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/LoginRequestDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos.AuthValidations
+{
+    public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+    {
+        public LoginRequestDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .NotNull().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .NotNull().WithMessage("Password is required.");
+        }
+    }
+}
