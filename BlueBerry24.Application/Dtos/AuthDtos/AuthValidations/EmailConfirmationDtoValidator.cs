dab68a222c99021c0ebf4701708f8587c8ade6a8
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-24 23:25:24 +0300

    Add EmailConfirmationDtoValidator for email confirmation validation
    Implemented a new validator for EmailConfirmationDto using FluentValidation. The validator enforces rules for the Email and Token fields, ensuring they are not null or empty and that the email follows a valid format. This addition enhances input validation for email confirmation requests.

---
+++ BlueBerry24.Application/Dtos/AuthDtos/AuthValidations/EmailConfirmationDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBerry24.Application.Dtos.AuthDtos.AuthValidations
+{
+    public class EmailConfirmationDtoValidator : AbstractValidator<EmailConfirmationDto>
+    {
+        public EmailConfirmationDtoValidator()
+        {
+            RuleFor(em => em.Email)
+                .NotNull().WithMessage("Email is required.")
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email must be a valid email address.");
+
+            RuleFor(em => em.Token)
+                .NotNull().WithMessage("Token is required.")
+                .NotEmpty().WithMessage("Token is required.");
+        }
+    }
+}
