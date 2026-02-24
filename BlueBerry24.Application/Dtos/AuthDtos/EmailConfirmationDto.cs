e881ef74d0c961e4f57a9de34fb1432faaa592d2
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-02-25 00:50:42 +0300

    Remove unnecessary data annotations from EmailConfirmationDto properties

--- BlueBerry24.Application/Dtos/AuthDtos/EmailConfirmationDto.cs
+++ BlueBerry24.Application/Dtos/AuthDtos/EmailConfirmationDto.cs
@@ -4,13 +4,7 @@ namespace BlueBerry24.Application.Dtos.AuthDtos
 {
     public class EmailConfirmationDto
     {
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
-        [Required]
         public string Token { get; set; } = string.Empty;
-
     }
 }
