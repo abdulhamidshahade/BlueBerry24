35dbd45c2c719615ea833b28e6a6fab3c7e5bf16
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-03-04 23:41:10 +0300

    Refactor UpdateUserDto to simplify validation attributes
    Removed unnecessary validation attributes from the UpdateUserDto class, specifically the Required and EmailAddress annotations for the Email property. This change streamlines the user update process by reducing validation complexity while preserving the essential structure of the DTO.

--- BlueBerry24.Application/Dtos/AuthDtos/UpdateUserDto.cs
+++ BlueBerry24.Application/Dtos/AuthDtos/UpdateUserDto.cs
@@ -4,17 +4,10 @@ namespace BlueBerry24.Application.Dtos.AuthDtos
 {
     public class UpdateUserDto
     {
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
-
-        [Required]
         public string UserName { get; set; }
-
         public string? FirstName { get; set; }
-
         public string? LastName { get; set; }
-
         public bool EmailConfirmed { get; set; }
     }
 }
