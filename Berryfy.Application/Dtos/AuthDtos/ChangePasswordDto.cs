1f12532e72b1ab597210cb2f84c28bcda5beb00b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-04-22 00:25:54 +0300

    Add DTOs for password change and profile update
    Created ChangePasswordDto and UpdateProfileDto classes to facilitate user password changes and profile updates. These DTOs include properties for current and new passwords, as well as user first name, last name, and username, enhancing the user account management functionality.

---
+++ Berryfy.Application/Dtos/AuthDtos/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+namespace Berryfy.Application.Dtos.AuthDtos
+{
+    public class ChangePasswordDto
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
---
+++ Berryfy.Application/Dtos/AuthDtos/UpdateProfileDto.cs
@@ -0,0 +1,9 @@
+namespace Berryfy.Application.Dtos.AuthDtos
+{
+    public class UpdateProfileDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
