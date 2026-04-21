d3a9874b3f66d0298312cd752e7b9f9123ade810
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-04-21 22:54:34 +0300

    Add migration for email confirmation code and refactor its database column

---
+++ Berryfy.Infrastructure/Migrations/20260421194602_refactor_db_10.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Berryfy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactor_db_10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailConfirmationCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailConfirmationCode",
+                table: "AspNetUsers",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
