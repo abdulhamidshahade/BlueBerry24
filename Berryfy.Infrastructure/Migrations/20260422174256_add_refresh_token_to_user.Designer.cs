7c2b3504d6590d1fababf2ece17802189ec85654
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-04-22 20:44:38 +0300

    Add refresh token fields to AspNetUsers table
    Added `RefreshToken` and `RefreshTokenExpiry` columns to the `AspNetUsers` table via EF Core migration to support storing user refresh tokens and their expiration. Updated migration files and model snapshot accordingly.

--- Berryfy.Infrastructure/Migrations/20260422174256_add_refresh_token_to_user.Designer.cs
+++ Berryfy.Infrastructure/Migrations/20260422174256_add_refresh_token_to_user.Designer.cs
@@ -4,6 +4,7 @@ using Berryfy.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Berryfy.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260422174256_add_refresh_token_to_user")]
+    partial class add_refresh_token_to_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
