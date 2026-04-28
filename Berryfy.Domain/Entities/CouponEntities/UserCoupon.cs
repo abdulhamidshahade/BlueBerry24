8f2578a76c579f65ccc9bfd2395978916331f3f7
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-04-28 20:43:39 +0300

    Remove unused using directives in UserCoupon.cs
    Cleaned up UserCoupon.cs by removing unnecessary using statements for System, Collections.Generic, Linq, Text, and Threading.Tasks. This improves code readability and maintainability.

--- Berryfy.Domain/Entities/CouponEntities/UserCoupon.cs
+++ Berryfy.Domain/Entities/CouponEntities/UserCoupon.cs
@@ -1,11 +1,6 @@
 
 using Berryfy.Domain.Entities.AuthEntities;
 using Berryfy.Domain.Entities.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Berryfy.Domain.Entities.CouponEntities
 {
